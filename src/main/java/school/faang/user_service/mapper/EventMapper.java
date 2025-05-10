9f2bef4a49a83442f823fca004ab89c125d30cbe
Gafurova Faribo <faribo.gafur@gmail.com>
2025-05-10 22:59:21 +0300

    метод поиска событий по фильтрам

--- src/main/java/school/faang/user_service/mapper/EventMapper.java
+++ src/main/java/school/faang/user_service/mapper/EventMapper.java
@@ -48,4 +48,10 @@ public interface EventMapper {
                 .map(this::eventToEventResponse)
                 .toList();
     }
+
+    default List<EventCreationResponse> toEventCreationResponses(List<Event> events) {
+        return events.stream()
+                .map(this::toEventCreationResponse)
+                .toList();
+    }
 }
