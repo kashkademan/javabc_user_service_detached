8a3e642dc78b8a1d2dfaaac96d583590b365fe13
Gafurova Faribo <faribo.gafur@gmail.com>
2025-05-11 13:36:28 +0300

    метод получения событий пользователя

--- src/main/java/school/faang/user_service/mapper/EventMapper.java
+++ src/main/java/school/faang/user_service/mapper/EventMapper.java
@@ -42,10 +42,4 @@ public interface EventMapper {
                 .map(this::eventToEventResponse)
                 .toList();
     }
-
-    default List<EventCreationResponse> toEventCreationResponses(List<Event> events) {
-        return events.stream()
-                .map(this::toEventCreationResponse)
-                .toList();
-    }
 }
