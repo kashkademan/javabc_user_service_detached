0bc3e86461385777341a5822201a2c992ddb882c
tanufa 25 <tanufa25@gmail.com>
2025-05-12 17:01:53 +0300

    BJS2-77981 Получение списка предложенных скиллов пользователя. Поправила получение скиллов: теперь вывод для текущего пользователя

--- src/main/java/school/faang/user_service/controller/SkillController.java
+++ src/main/java/school/faang/user_service/controller/SkillController.java
@@ -5,12 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.skill.CreateSkillDto;
+import school.faang.user_service.dto.skill.SkillCandidateDto;
 import school.faang.user_service.dto.skill.SkillDto;
 import school.faang.user_service.entity.Skill;
 import school.faang.user_service.exception.DataValidationException;
@@ -33,7 +33,7 @@ public class SkillController {
         try {
             Skill skill = skillMapper.toEntity(skillDto);
             skill = skillService.create(skill);
-            CreateSkillDto skillDtoCreated = skillMapper.toDto(skill);
+            CreateSkillDto skillDtoCreated = skillMapper.toCreateDto(skill);
             log.info("Созданный навык: {}", skillDtoCreated);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
@@ -45,10 +45,18 @@ public class SkillController {
         }
     }
 
-    @GetMapping("/{userId}")
-    public ResponseEntity<?> getUserSkills(@PathVariable Long userId) {
-        List<SkillDto> skillDtoList = skillMapper.toDtos(skillService.getUserSkills(userId));
-        log.info("У пользователя {} есть навыки {}", userId, skillDtoList);
+    @GetMapping
+    public ResponseEntity<?> getUserSkills() {
+        List<SkillDto> skillDtoList = skillMapper.toDtos(skillService.getUserSkills());
+        log.info("Полученные навыки {}", skillDtoList);
         return ResponseEntity.ok(skillDtoList);
     }
+
+    @GetMapping("/offered")
+    public ResponseEntity<?> getOfferedSkills() {
+        List<SkillCandidateDto> offeredSkillDtoList = skillMapper.toOfferedDtos(skillService.getOfferedSkills());
+        log.info("Предложенные навыки {}", offeredSkillDtoList);
+        return ResponseEntity.ok(offeredSkillDtoList);
+    }
+
 }
