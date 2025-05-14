9fabe0b5669ea98a7e9af5d1f0f4bce2ab16945c
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-14 20:35:47 +0200

    Task 77385 tests init

---
+++ src/test/java/school/faang/user_service/service/goal/GoalServiceImplTest.java
@@ -0,0 +1,38 @@
+package school.faang.user_service.service.goal;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GoalServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createGoal() {
+    }
+
+    @Test
+    void updateGoal() {
+    }
+
+    @Test
+    void deleteGoal() {
+    }
+
+    @Test
+    void findSubtasksByGoalId() {
+    }
+
+    @Test
+    void findGoalsByUserId() {
+    }
+}
