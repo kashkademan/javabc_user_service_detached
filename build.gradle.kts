b689acf3074e4f5e618000e2b97fd7e81436335d
a.dernovskiy <dernovski@icloud.com>
2025-05-14 13:39:18 +0700

    fixed comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,7 +123,7 @@ tasks.jacocoTestReport {
                       "**/*Mapper.class/**",
                       "**/*MapperImpl.class/**",
                       "**/entity/**",
-                      "**/controler/**",
+                      "**/controller/**",
                       "**/repository/**",
                       "**/json/student/**"
                    )
