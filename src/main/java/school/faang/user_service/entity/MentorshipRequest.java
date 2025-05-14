4d34dff133500844ef7e78cd5b9a95e204c18148
a.dernovskiy <dernovski@icloud.com>
2025-05-14 18:28:44 +0700

    added fetch.LAZY in MentorshipRequest

--- src/main/java/school/faang/user_service/entity/MentorshipRequest.java
+++ src/main/java/school/faang/user_service/entity/MentorshipRequest.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,11 +35,11 @@ public class MentorshipRequest {
     @Column(name = "description", nullable = false, length = 4096)
     private String description;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "requester_id", nullable = false)
     private User requester;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "receiver_id", nullable = false)
     private User receiver;
 
