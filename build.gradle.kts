22c6c527a47ae8e07067d7ea533bf670c9f250a9
a.dernovskiy <dernovski@icloud.com>
2025-05-15 13:46:14 +0700

    add checkStyle

--- build.gradle.kts
+++ build.gradle.kts
@@ -131,7 +131,7 @@ tasks.jacocoTestReport {
                       "**/*Mapper.class/**",
                       "**/*MapperImpl.class/**",
                       "**/entity/**",
-                      "**/controller/**",
+                      "**/controler/**",
                       "**/repository/**",
                       "**/json/student/**"
                    )
