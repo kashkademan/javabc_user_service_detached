b3c75cb8bf8d58415ed052ea26b3149da6296fc1
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-05-17 17:09:01 +0300

    BJS2-78237 add test

---
+++ src/test/java/school/faang/user_service/service/TestTest.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
