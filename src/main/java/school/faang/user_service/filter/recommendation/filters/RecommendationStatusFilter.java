3daee02aed59e2e2544b17573fd5867b572164bd
Aleksandr <alexandr.sidorkin@gmail.com>
2025-05-17 14:35:48 +0200

    77391 тесты для 77371 - совместная работа над целью, init с исправлениями запуска

--- src/main/java/school/faang/user_service/filter/recommendation/filters/RecommendationStatusFilter.java
+++ src/main/java/school/faang/user_service/filter/recommendation/filters/RecommendationStatusFilter.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
-public class StatusFilter implements RecommendationRequestFilterStrategy {
+public class RecommendationStatusFilter implements RecommendationRequestFilterStrategy {
 
     @Override
     public boolean isApplicable(RequestFilterDto requestFilterDto) {
