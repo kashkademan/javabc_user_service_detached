4bfdfa3e89f1ec58fea8d3e672a65562b95d2f33
Tamirlan Dexengazy <dextomi02@gmail.com>
2025-05-17 12:25:55 +0500

    Создал эндпоинты, добавил больше проверок для дат, для каждого метода создал отдельные ДТО, добавил в них же валидацию, сделал поиск юзера через userContext

--- src/main/java/school/faang/user_service/dto/career/CareerResponseDto.java
+++ src/main/java/school/faang/user_service/dto/career/CareerResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDate;
 
 @Data
-public class CareerDto {
+public class CareerResponseDto {
     private long id;
     private LocalDate from;
     private LocalDate to;
