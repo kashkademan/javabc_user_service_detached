8b701beb267a8e3696fa1ad7d0fbdddaf36361c8
Tamirlan Dexengazy <dextomi02@gmail.com>
2025-05-17 12:25:55 +0500

    Создал эндпоинты, добавил больше проверок для дат, для каждого метода создал отдельные ДТО, добавил в них же валидацию, сделал поиск юзера через userContext

--- src/main/java/school/faang/user_service/controller/career/CareerController.java
+++ src/main/java/school/faang/user_service/controller/career/CareerController.java
@@ -1,32 +1,56 @@
 package school.faang.user_service.controller.career;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import school.faang.user_service.dto.career.CareerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.career.CareerCreateDto;
+import school.faang.user_service.dto.career.CareerResponseDto;
+import school.faang.user_service.dto.career.CareerUpdateDto;
 import school.faang.user_service.entity.Career;
 import school.faang.user_service.mapper.career.CareerMapper;
 import school.faang.user_service.service.career.CareerService;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/user/career")
 public class CareerController {
     private final CareerService careerService;
     private final CareerMapper careerMapper;
 
-    public CareerDto addCareer(long userId, CareerDto careerDto) {
-        Career career = careerMapper.toCareer(careerDto);
-        Career saved = careerService.addCareer(userId, career);
-        return careerMapper.toCareerDto(saved);
+    @PostMapping("/add")
+    public ResponseEntity<CareerResponseDto> addCareer(
+            @Valid @RequestBody CareerCreateDto careerCreateDto) {
+        Career career = careerMapper.toCareer(careerCreateDto);
+        Career saved = careerService.addCareer(career);
+        return new ResponseEntity<>(
+                careerMapper.toCareerResponseDto(saved),
+                HttpStatus.CREATED);
     }
 
-    public CareerDto updateCareer(long userId, CareerDto careerDto) {
-        Career career = careerMapper.toCareer(careerDto);
-        Career saved = careerService.updateCareer(userId, career);
-        return careerMapper.toCareerDto(saved);
+    @PatchMapping("/update")
+    public ResponseEntity<CareerResponseDto> updateCareer(
+            @Valid @RequestBody CareerUpdateDto careerUpdateDto) {
+        Career career = careerMapper.toCareer(careerUpdateDto);
+        Career updated = careerService.updateCareer(career);
+        return new ResponseEntity<>(
+                careerMapper.toCareerResponseDto(updated),
+                HttpStatus.OK);
     }
 
-    public CareerDto getById(long careerId) {
+    @GetMapping("/{careerId}")
+    public ResponseEntity<CareerResponseDto> getById(
+            @PathVariable("careerId") long careerId) {
         Career career = careerService.getById(careerId);
-        return careerMapper.toCareerDto(career);
+        return new ResponseEntity<>(
+                careerMapper.toCareerResponseDto(career),
+                HttpStatus.OK);
     }
 }
