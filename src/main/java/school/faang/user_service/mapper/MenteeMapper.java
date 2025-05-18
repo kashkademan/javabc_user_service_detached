2f60767518ece141706440d451fdfdd78c32cf59
P-Lidia <Lydiasmolyakova@gmail.com>
2025-05-18 16:04:32 +0200

    Fix MenteeMapper and MentorMapper classes

--- src/main/java/school/faang/user_service/mapper/MenteeMapper.java
+++ src/main/java/school/faang/user_service/mapper/MenteeMapper.java
@@ -1,11 +1,14 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import school.faang.user_service.dto.mentorship.MenteeDto;
 import school.faang.user_service.entity.User;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MenteeMapper {
-    MenteeDto toDto (User user);
+
+    @Mapping(source = "username", target = "name")
+    MenteeDto toDto(User user);
 }
--- src/main/java/school/faang/user_service/mapper/MentorMapper.java
+++ src/main/java/school/faang/user_service/mapper/MentorMapper.java
@@ -1,11 +1,14 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import school.faang.user_service.dto.mentorship.MentorDto;
 import school.faang.user_service.entity.User;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface MentorMapper {
+
+    @Mapping(source = "username", target = "name")
     MentorDto toDto(User user);
 }
