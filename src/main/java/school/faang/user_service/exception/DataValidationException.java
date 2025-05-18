4f237368e4b4b6746d5bdd8510cb41c1397bb624
Sergey Filippovich <filippovich.sergey@gmail.com>
2025-05-18 11:23:42 +0400

    classes EducationController and EducationService: added the @RequiredArgsContructor annotation class DataValidationException: moved to a new package "exception" class Education: changed annotations @Getter and @Setter to @Data class EducationController and EducationService: changed "long" to "Long" for ids. class EducationService: added the method for checking if id is null. class EducationService, method checkYearFrom: added "compare" method. class EducationService, method saveEducation: updated the return data.

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.service.education;
+package school.faang.user_service.exception;
 
 public class DataValidationException extends Throwable {
     public DataValidationException() {
--- src/main/java/school/faang/user_service/service/education/EducationService.java
+++ src/main/java/school/faang/user_service/service/education/EducationService.java
@@ -1,10 +1,11 @@
 package school.faang.user_service.service.education;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import school.faang.user_service.dto.EducationDto;
 import school.faang.user_service.entity.Education;
 import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.repository.UserRepository;
 import school.faang.user_service.repository.EducationRepository;
 import school.faang.user_service.mapper.EducationMapper;
@@ -13,25 +14,20 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class EducationService {
     private final UserRepository userRepository;
     private final EducationRepository educationRepository;
     private final EducationMapper educationMapper;
 
-    @Autowired
-    public EducationService(UserRepository userRepository, EducationRepository educationRepository, EducationMapper educationMapper) {
-        this.userRepository = userRepository;
-        this.educationRepository = educationRepository;
-        this.educationMapper = educationMapper;
-    }
-
     public void checkYearFrom(EducationDto educationDto) throws DataValidationException {
-        if (educationDto.getYearFrom() > LocalDate.now().getYear()) {
-            throw new DataValidationException("Your year is greater than the current year.");
-        }//The task is to check if the yearFrom is less than the current year.
+        if (!(educationDto.getYearFrom().compareTo(LocalDate.now().getYear()) > 0)) {
+            return;//The task is to check if the yearFrom is less than the current year.
+        }
+        throw new DataValidationException("Your year is greater than the current year.");
     }    //However, the yearFrom can be equal to the current year.
 
-    public Optional<User> checkUserIdEmpty(long userId) throws DataValidationException {
+    public Optional<User> checkUserIdEmpty(Long userId) throws DataValidationException {
         Optional<User> userOptional = userRepository.findById(userId);
         if (userOptional.isEmpty()) {
             throw new DataValidationException("User with ID " + userId + " not found");
@@ -39,25 +35,35 @@ public class EducationService {
         return userOptional;
     }
 
-    public EducationDto saveEducation(long userId, EducationDto educationDto) throws DataValidationException {
+    public void checkUserIdNull(Long userId) throws DataValidationException {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            throw new DataValidationException("User ID is null.");
+        }
+    }
+
+    public EducationDto saveEducation(Long userId, EducationDto educationDto) throws DataValidationException {
         checkYearFrom(educationDto);
+        checkUserIdNull(userId);
         Optional<User> userOptional = checkUserIdEmpty(userId);
 
         Education education = educationMapper.toEducation(educationDto);
         education.setUser(userOptional.get());
 
-        educationRepository.save(education);
-        return educationMapper.toEducationDto(education);
+        Education resultEducation = educationRepository.save(education);
+        return educationMapper.toEducationDto(resultEducation);
     }
 
-    public EducationDto addEducation(long userId, EducationDto educationDto) throws DataValidationException {
+    public EducationDto addEducation(Long userId, EducationDto educationDto) throws DataValidationException {
         checkYearFrom(educationDto);
+        checkUserIdNull(userId);
 
         return saveEducation(userId, educationDto);
     }
 
-    public EducationDto updateEducation(long userId, EducationDto educationDto) throws DataValidationException {
+    public EducationDto updateEducation(Long userId, EducationDto educationDto) throws DataValidationException {
         checkYearFrom(educationDto);
+        checkUserIdNull(userId);
 
         if (userId != educationDto.getId()) {
             throw new DataValidationException("User ID in the path does not match the user ID in the request body");
@@ -66,7 +72,7 @@ public class EducationService {
         return saveEducation(userId, educationDto);
     }
 
-    public EducationDto getById(long educationId) throws DataValidationException {
+    public EducationDto getById(Long educationId) throws DataValidationException {
         Education education = educationRepository.findById(educationId)
                 .orElseThrow(() -> new DataValidationException("Education with ID " + educationId + " not found"));
         return educationMapper.toEducationDto(education);
