fd22180616ca8d284dd341087ff7f2feb2db0f8c
Robert Abdrakhmanov <rap215@icloud.com>
2025-05-18 15:42:44 +0300

    TestUserEducation

--- src/main/java/school/faang/user_service/client/controller/education/EducationController.java
+++ src/main/java/school/faang/user_service/client/controller/education/EducationController.java
@@ -1,9 +1,6 @@
 package school.faang.user_service.client.controller.education;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.client.service.education.EducationServiceImpl;
 import school.faang.user_service.dto.EducationDto;
@@ -13,17 +10,14 @@ import school.faang.user_service.dto.EducationDto;
 public class EducationController {
     private final EducationServiceImpl educationService;
 
-    @PostMapping
     public EducationDto addEducation(long userId, EducationDto educationDto) {
         return educationService.addEducation(userId, educationDto);
     }
 
-    @PutMapping
     public EducationDto updateEducation(long userId, EducationDto educationDto) {
         return educationService.updateEducation(userId, educationDto);
     }
 
-    @GetMapping
     public EducationDto getById(long educationId) {
         return educationService.getById(educationId);
     }
---
+++ src/test/java/school/faang/user_service/service/controller/EducationServiceTest.java
@@ -0,0 +1,133 @@
+package school.faang.user_service.service.controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import school.faang.user_service.client.service.education.EducationServiceImpl;
+import school.faang.user_service.dto.EducationDto;
+import school.faang.user_service.entity.Education;
+import school.faang.user_service.entity.User;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.mapper.EducationMapperImpl;
+import school.faang.user_service.repository.EducationRepository;
+import school.faang.user_service.repository.UserRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EducationServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private EducationRepository educationRepository;
+
+    @Spy
+    private EducationMapperImpl educationMapper;
+
+    @InjectMocks
+    private EducationServiceImpl educationService;
+
+    @Captor
+    private ArgumentCaptor<Education> educationCaptor;
+
+    private long userId;
+    private long educationId;
+    private EducationDto educationDto;
+    private int correctYearFrom;
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        userId = 1L;
+        educationId = 12L;
+        correctYearFrom = 1923;
+        educationDto = EducationDto.builder()
+                .yearFrom(correctYearFrom)
+                .id(educationId)
+                .build();
+        user = User.builder()
+                .id(userId)
+                .build();
+
+
+    }
+
+    @Test
+    void testAddEducationIncorrectYear() {
+        int incorrectYear = 2026;
+        educationDto.setYearFrom(incorrectYear);
+
+        DataValidationException dataValidationException = assertThrows(DataValidationException.class,
+                () -> educationService.addEducation(userId, educationDto));
+        assertEquals("Year of start must be early", dataValidationException.getMessage());
+
+    }
+
+    @Test
+    void testAddEducationIncorrectUserId() {
+        EntityNotFoundException entityNotFoundException = assertThrows(EntityNotFoundException.class,
+                () -> educationService.addEducation(userId, educationDto));
+        assertEquals(String
+                .format("User with id %d was not found", userId), entityNotFoundException.getMessage());
+    }
+
+    @Test
+    void testAddEducationRepositorySave() {
+        when(userRepository.findById(userId)).thenReturn(Optional.ofNullable(user));
+
+        educationService.addEducation(userId, educationDto);
+
+        verify(educationRepository, times(1)).save(educationCaptor.capture());
+        assertEquals(educationDto.getYearFrom(), educationCaptor.getValue().getYearFrom());
+        assertEquals(educationDto.getId(), educationCaptor.getValue().getId());
+    }
+
+    @Test
+    void testUpdateEducationNotExistEducation() {
+
+        EntityNotFoundException entityNotFoundException = assertThrows(EntityNotFoundException.class,
+                () -> educationService.updateEducation(userId, educationDto));
+        assertEquals(String.format("Education by id %d was not found!", educationId)
+                , entityNotFoundException.getMessage());
+    }
+
+    @Test
+    void testUpdateEducationIncorrectUserId() {
+        when(educationRepository.existsById(educationId)).thenReturn(true);
+        EntityNotFoundException entityNotFoundException = assertThrows(EntityNotFoundException.class,
+                () -> educationService.updateEducation(userId, educationDto));
+        assertEquals(String
+                .format("User with id %d was not found", userId), entityNotFoundException.getMessage());
+    }
+@Test
+    void testUpdateEducationRepositorySave() {
+        when(userRepository.findById(userId)).thenReturn(Optional.ofNullable(user));
+        when(educationRepository.existsById(educationId)).thenReturn(true);
+
+        educationService.updateEducation(userId, educationDto);
+
+        verify(educationRepository, times(1)).save(educationCaptor.capture());
+        assertEquals(userId, educationCaptor.getValue().getUser().getId());
+    }
+
+    @Test
+    void testGetByIdIncorrectUser() {
+
+        EntityNotFoundException entityNotFoundException = assertThrows(EntityNotFoundException.class,
+                () -> educationService.getById(educationId));
+        assertEquals(String
+                .format("Education with id %d was not found", educationId), entityNotFoundException.getMessage());
+    }
+
+}
+
+
