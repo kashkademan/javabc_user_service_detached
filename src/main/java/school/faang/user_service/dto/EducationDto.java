7432e19746631dcffb42df6a386f43daf87a2b4c
Robert Abdrakhmanov <rap215@icloud.com>
2025-05-18 15:42:44 +0300

    TestUserEducation

--- src/main/java/school/faang/user_service/dto/EducationDto.java
+++ src/main/java/school/faang/user_service/dto/EducationDto.java
@@ -1,8 +1,10 @@
 package school.faang.user_service.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class EducationDto {
     private long id;
     private Integer yearFrom;
