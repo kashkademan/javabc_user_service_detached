66e725339a84ffaae575c3222a3e33b10ac4c07b
Vladimir Levshin <levshin.v.99@gmail.com>
2025-05-21 21:19:23 +0300

    BJS2-77487: Added swagger for events API.

--- src/main/java/school/faang/user_service/dto/event/EventDto.java
+++ src/main/java/school/faang/user_service/dto/event/EventDto.java
@@ -1,5 +1,8 @@
 package school.faang.user_service.dto.event;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import school.faang.user_service.entity.event.EventStatus;
 import school.faang.user_service.entity.event.EventType;
@@ -7,12 +10,20 @@ import school.faang.user_service.entity.event.EventType;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Schema(description = "Event entity")
 @Data
 public class EventDto {
+    @Schema(description = "Unique id of event", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
+    @Schema(description = "Title of the event", example = "Title", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotBlank
     private String title;
+    @Schema(description = "Start date of the event", example = "2025-01-01T00:00", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    @Schema(description = "Owner id of the event", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    @NotNull
     private Long ownerId;
     private String description;
     private List<Long> relatedSkills;
