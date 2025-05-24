1ab6e1b9ccd2a9a5b4a7b7bf298d9af4e7ab85d1
Robert Abdrakhmanov <rap215@icloud.com>
2025-05-24 16:22:20 +0300

    Fix

--- src/test/java/school/faang/user_service/service/controller/EducationControllerTest.java
+++ src/test/java/school/faang/user_service/service/controller/EducationControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import school.faang.user_service.client.controller.education.EducationController;
+import school.faang.user_service.controller.education.EducationController;
 import school.faang.user_service.client.service.education.EducationService;
 import school.faang.user_service.dto.EducationDto;
 
