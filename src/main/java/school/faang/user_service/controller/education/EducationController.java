9351d624cc2b17e32a83891f3dd07eace8b998e4
Robert Abdrakhmanov <rap215@icloud.com>
2025-05-24 18:20:53 +0300

    fix test

--- src/main/java/school/faang/user_service/controller/education/EducationController.java
+++ src/main/java/school/faang/user_service/controller/education/EducationController.java
@@ -2,13 +2,13 @@ package school.faang.user_service.controller.education;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
-import school.faang.user_service.client.service.education.EducationServiceImpl;
+import school.faang.user_service.client.service.education.EducationService;
 import school.faang.user_service.dto.EducationDto;
 
 @RestController
 @RequiredArgsConstructor
 public class EducationController {
-    private final EducationServiceImpl educationService;
+    private final EducationService educationService;
 
     public EducationDto addEducation(long userId, EducationDto educationDto) {
         return educationService.addEducation(userId, educationDto);
