297fc8283daed1e5e46a221c2c2356be4d3d7396
Artmasterrr <artmasterrr@hotmail.com>
2025-05-25 19:47:49 +0200

    Test

--- src/main/java/school/faang/user_service/dto/CareerDto.java
+++ src/main/java/school/faang/user_service/dto/CareerDto.java
@@ -1,9 +1,11 @@
 package school.faang.user_service.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
+@Builder(toBuilder = true)
 @Data
 public class CareerDto {
     private Long id;
