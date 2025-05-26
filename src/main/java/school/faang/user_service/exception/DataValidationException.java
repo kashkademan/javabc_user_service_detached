a0015e23532f07e88b4c5741a2d8cfc9006a1294
Artmasterrr <artmasterrr@hotmail.com>
2025-05-26 20:27:35 +0200

    Commit

--- src/main/java/school/faang/user_service/exception/DataValidationException.java
+++ src/main/java/school/faang/user_service/exception/DataValidationException.java
@@ -4,6 +4,7 @@ public class DataValidationException extends RuntimeException {
     public DataValidationException(String message, Throwable cause) {
         super(message, cause);
     }
+
     public DataValidationException(String message) {
         super(message);
     }
