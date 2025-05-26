31a108d91ad8d59c64d14a33324f3729b30e7610
a.dernovskiy <dernovski@icloud.com>
2025-05-26 15:31:30 +0700

    first version

--- src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
+++ src/test/java/school/faang/user_service/service/MentorshipRequestServiceTest.java
@@ -2,21 +2,40 @@ package school.faang.user_service.service;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 import school.faang.user_service.entity.MentorshipRequest;
+import school.faang.user_service.mapper.mentorship.MentorshipResponseMapper;
 import school.faang.user_service.repository.mentorship.MentorshipRequestRepository;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@ExtendWith(MockitoExtension.class)
 public class MentorshipRequestServiceTest {
 
     @Mock
     private MentorshipRequestRepository mentorshipRequestRepository;
 
+    @InjectMocks
+    private MentorshipRequestService mentorshipRequestService;
+
+    @Spy
+    private MentorshipResponseMapper mentorshipResponseMapper;
+
+
+
     @Test
-    @DisplayName("Запрос успешно создан")
-    public void testRequestCreateSuccessfuly() {
-        MentorshipRequest request = mentorshipRequestRepository.create(5, 6, "Тестовое описание");
-        assertNotNull(request);
+    @DisplayName("Проверка сохранения запроса в БД")
+    public void testRequestIsSaved() {
+
+
+        Mockito.when(mentorshipRequestRepository.create(, 5, "Это тестовое описание"))
+                .thenReturn(new MentorshipRequest());
+
+        mentorshipRequestService.requestMentorship();
     }
 }
