8d872e894e3b66f468d7bc74277dbe5f51639f1d
Konstantin Snurnicyn <k.snurnitsyn@hh.ru>
2025-05-27 16:37:40 +0500

    Test Jacoco

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,7 @@ jsonSchema2Pojo {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport, tasks.jacocoTestCoverageVerification)
 }
 
 val test by tasks.getting(Test::class) { testLogging.showStandardStreams = true }
