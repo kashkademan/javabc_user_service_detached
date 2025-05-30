046198be24c7bf71ca1d8fcdc8dab4bd6f57cee9
pirojok_yt <tamik34@mail.ru>
2025-05-30 14:32:55 +0500

    applied changes

--- src/main/java/school/faang/user_service/controller/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/RecommendationController.java
@@ -1,38 +1,44 @@
 package school.faang.user_service.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.exceptions.DataValidationException;
 import school.faang.user_service.service.RecommendationService;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
-    public RecommendationDto giveRecommendation(RecommendationDto recommendationDto) {
+    @PostMapping("/post")
+    public RecommendationDto giveRecommendation(@RequestBody RecommendationDto recommendationDto) {
         validate(recommendationDto);
         return recommendationService.create(recommendationDto);
     }
 
-    public RecommendationDto updateRecommendation(RecommendationDto recommendationDto) {
+    @PutMapping("/update")
+    public RecommendationDto updateRecommendation(@RequestBody RecommendationDto recommendationDto) {
         validate(recommendationDto);
 
         return recommendationService.update(recommendationDto);
     }
 
+    @DeleteMapping("/delete")
     public void deleteRecommendation(long id) {
         recommendationService.delete(id);
     }
 
-    public List<RecommendationDto> getAllUserRecommendations(long receiverId) {
+    @GetMapping("/userRecommendations/{receiverId}")
+    public List<RecommendationDto> getAllUserRecommendations(@PathVariable long receiverId) {
         return recommendationService.getAllUserRecommendations(receiverId);
     }
 
-    public List<RecommendationDto> getAllGivenRecommendations(long id) {
+    @GetMapping("/givenRecommendations/{id}")
+    public List<RecommendationDto> getAllGivenRecommendations(@PathVariable long id) {
         return recommendationService.getAllGivenRecommendations(id);
     }
 
