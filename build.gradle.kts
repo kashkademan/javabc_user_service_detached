5df25fa9799e9d08094d863fbead20d3cc60eca6
Tamirlan Dexengazy <dextomi02@gmail.com>
2025-06-09 04:32:51 +0500

    Надеюсь получилось поправить тесты

--- build.gradle.kts
+++ build.gradle.kts
@@ -76,9 +76,11 @@ dependencies {
     /**
      * Tests
      */
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.3")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 
     /**
      * Swagger
