24526f1fa41f76c7b5e1f796d7bd76b182f84b0c
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-06-09 15:39:50 +0300

    BJS2-78227 refactor

--- src/main/java/school/faang/user_service/client/DiceBearClient.java
+++ src/main/java/school/faang/user_service/client/DiceBearClient.java
@@ -4,7 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "dice-bear", url = "https://${services.dice-bear.domain}/${services.dice-bear.version}")
+@FeignClient(name = "dice-bear",
+        url = "https://${services.dice-bear.domain}/${services.dice-bear.version}",
+        configuration = FeignConfig.class)
 public interface DiceBearClient {
     @GetMapping(value = "/pixel-art/svg", produces = "image/svg+xml")
     byte[] getRandomAvatar(@RequestParam(required = false) String seed);
