fbc11c35256daa889045549ec18f5ad9704df21b
pirojok_yt <tamik34@mail.ru>
2025-06-09 20:33:23 +0500

    code style fix

--- src/main/java/school/faang/user_service/controller/UserController.java
+++ src/main/java/school/faang/user_service/controller/UserController.java
@@ -1,12 +1,10 @@
 package school.faang.user_service.controller;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.UserDto;
@@ -22,8 +20,7 @@ public class UserController {
     private final UserMapper userMapper;
 
     @GetMapping("/{userId}")
-    public UserDto getUser(@PathVariable("userId") @Min(value = 1, message = "id must be a positive number") long id,
-                           @RequestHeader("x-user-id") @NotBlank(message = "Field cannot be blank") String xUserId) {
+    public UserDto getUser(@PathVariable("userId") @Min(value = 1, message = "id must be a positive number") long id) {
         return userMapper.toDto(userService.getUserById(id));
     }
 }
