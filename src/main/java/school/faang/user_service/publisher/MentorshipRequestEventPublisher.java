d7ccd5d94b25940caa444ed303ac61a2ed1b6cd0
pirojok_yt <tamik34@mail.ru>
2025-06-11 23:50:27 +0500

    codestyle fix

--- src/main/java/school/faang/user_service/publisher/MentorshipRequestEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/MentorshipRequestEventPublisher.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -12,7 +11,6 @@ import school.faang.user_service.dto.event.MentorshipRequestEvent;
 public class MentorshipRequestEventPublisher {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private final ObjectMapper objectMapper;
 
     @SneakyThrows
     public void publish(MentorshipRequestEvent event) {
