fafbb634cf48b7b6a7a3be068eba9cadc25e1756
pirojok_yt <tamik34@mail.ru>
2025-06-11 23:33:22 +0500

    created publisher

--- src/main/java/school/faang/user_service/dto/UserDto.java
+++ src/main/java/school/faang/user_service/dto/UserDto.java
@@ -1,4 +1,6 @@
 package school.faang.user_service.dto;
 
-public record UserDto(Long id, String username, String email) {
+import java.util.Locale;
+
+public record UserDto(Long id, String username, String email, Locale locale) {
 }
