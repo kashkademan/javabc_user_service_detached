39bb34cfe39127c0c6e542e00d6fb7849571deee
trytofixme <semenov.nikita@phystech.edu>
2025-06-23 19:33:04 +0300

    Update KafkaEventTopicProperties.java

--- src/main/java/school/faang/user_service/config/kafka/KafkaEventTopicProperties.java
+++ src/main/java/school/faang/user_service/config/kafka/KafkaEventTopicProperties.java
@@ -3,12 +3,10 @@ package school.faang.user_service.config.kafka;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
-@ConfigurationProperties(prefix = "spring.data.kafka.topic.event")
-@Configuration
+@ConfigurationProperties(prefix = "spring.kafka.topic.event")
 public class KafkaEventTopicProperties {
     private String name;
     private int partitions;
