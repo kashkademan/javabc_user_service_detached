74556e1e5188469dcaa4a556089de2cc045d1130
Gigabyte <012_erasyl@mail.ru>
2025-06-24 13:40:21 +0500

    BJS2-76938

--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -1,10 +1,9 @@
 package school.faang.user_service.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import school.faang.user_service.dto.UserDto;
 import school.faang.user_service.entity.User;
-import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.UserMapper;
 import school.faang.user_service.repository.UserRepository;
 
@@ -12,10 +11,9 @@ import school.faang.user_service.repository.UserRepository;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
 
     public User getUserById(Long id) {
         return userRepository.findById(id)
-                .orElseThrow(() -> new DataValidationException("User not found"));
+                .orElseThrow(() -> new EntityNotFoundException("User with id = " + id + " doesn't exist"));
     }
 }
