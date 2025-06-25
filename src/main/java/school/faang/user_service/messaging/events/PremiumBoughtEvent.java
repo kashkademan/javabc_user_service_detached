8f6bdba6921355d0862dfeb904640e3f66ff66f7
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-25 21:12:49 +0300

    fix: pr comments

--- src/main/java/school/faang/user_service/messaging/events/PremiumBoughtEvent.java
+++ src/main/java/school/faang/user_service/messaging/events/PremiumBoughtEvent.java
@@ -6,5 +6,6 @@ import school.faang.user_service.enums.PremiumPeriod;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record PremiumBoughtEvent(Long userId, BigDecimal price, Currency currency, PremiumPeriod premiumPeriod, LocalDateTime startDate) {
+public record PremiumBoughtEvent(Long userId, BigDecimal price, Currency currency, PremiumPeriod premiumPeriod,
+                                 LocalDateTime startDate) {
 }
