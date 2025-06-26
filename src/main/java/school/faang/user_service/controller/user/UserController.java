ee909552f4c662732527dfa888ff616892f8fa83
Maria Sahno <sahnomyu@m2.ru>
2025-06-26 15:41:19 +0500

    BJS2-83566 added java doc

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.user.CreateUserDto;
 import school.faang.user_service.dto.user.UpdateUserDto;
@@ -14,35 +15,33 @@ public class UserController {
     private final UserService userService;
 
     public UserDto create(CreateUserDto userDto) {
-        if (userDto.username() == null || userDto.username().isBlank()) {
-            throw new DataValidationException("Username should be present!");
-        }
-        if (userDto.email() == null || userDto.email().isBlank()) {
-            throw new DataValidationException("Email should be present!");
-        }
-        if (userDto.password() == null || userDto.password().isBlank()) {
-            throw new DataValidationException("Password should be present!");
-        }
-        if (userDto.countryId() == null) {
-            throw new DataValidationException("Country should be present!");
-        }
+        validateString(userDto.username(), "username");
+        validateString(userDto.email(), "email");
+        validateString(userDto.password(), "password");
+        validateNotNull(userDto.countryId(), "country");
         return userService.create(userDto);
     }
 
     public UserDto update(long userId, UpdateUserDto userDto) {
-        if (userDto.username() == null || userDto.username().isBlank()) {
-            throw new DataValidationException("Username should be present!");
-        }
-        if (userDto.email() == null || userDto.email().isBlank()) {
-            throw new DataValidationException("Email should be present!");
-        }
-        if (userDto.countryId() == null) {
-            throw new DataValidationException("Country should be present!");
-        }
+        validateString(userDto.username(), "username");
+        validateString(userDto.email(), "email");
+        validateNotNull(userDto.countryId(), "country");
         return userService.update(userId, userDto);
     }
 
     public UserDto getById(long userId) {
         return userService.getById(userId);
+    }
+
+    private void validateString(String value, String paramName) {
+        if (StringUtils.isNotBlank(value)) {
+            throw new DataValidationException(paramName + " should be present!");
+        }
+    }
+
+    private void validateNotNull(Object value, String paramName) {
+        if (value == null) {
+            throw new DataValidationException(paramName + " should be present!");
+        }
     }
 }
