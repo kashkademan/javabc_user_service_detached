0c25aabede451cd14fc5b8bdede5c0013dec750b
Keriod <nikolainovopashin98@gmail.com>
2025-06-28 07:39:33 +0500

    BJS2-78140. Уведомление о достижении цели - Рефакторинг - Тест

--- src/test/java/school/faang/user_service/ApplicationContextTest.java
+++ src/test/java/school/faang/user_service/ApplicationContextTest.java
@@ -1,16 +1,10 @@
 package school.faang.user_service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
--- src/test/java/school/faang/user_service/publisher/GoalCompletePublisherIT.java
+++ src/test/java/school/faang/user_service/publisher/GoalCompletePublisherIT.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Import;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.ContextConfiguration;
