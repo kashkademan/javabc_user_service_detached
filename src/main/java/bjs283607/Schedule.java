4c6291e23bfdc00f7a39ad0e988eb103e6d71264
Askar <asklooka@gmail.com>
2025-07-02 18:41:05 -0400

    лодлридшидлоижолидшрмдииж

---
+++ src/main/java/bjs283607/Schedule.java
@@ -0,0 +1,15 @@
+package bjs283607;
+
+import lombok.EqualsAndHashCode;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@EqualsAndHashCode
+public class Schedule {
+    private Map<Map<String, Map<String, Map<Integer, Integer>>>, Integer> schedules = new HashMap<>();
+    //      График. день недели. Событие.  Время от. Время до.   Id
+
+    public void addSchedule(String day, )
+}
