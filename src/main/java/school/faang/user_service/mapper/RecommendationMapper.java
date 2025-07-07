78656ce47ae4ca0bdca78d4d513d8ffee6de6685
Ivi-Anastasia <qwwsoul@gmail.com>
2025-07-07 12:19:29 +0100

    Added @Mapping ignore for User field mappings

--- src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
+++ src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
@@ -1,11 +1,14 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.entity.recommendation.Recommendation;
 
 @Mapper(componentModel = "spring")
 public interface RecommendationMapper {
+    @Mapping(target = "authorId", ignore = true)
+    @Mapping(target = "receiverId", ignore = true)
     RecommendationDto toRecommendationDto(Recommendation recommendation);
 
 }
