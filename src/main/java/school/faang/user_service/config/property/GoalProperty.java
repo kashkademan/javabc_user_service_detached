7ce99b253d832a98bfe38d128071bbd59bc9164e
licosty <d.chigantseva@gmail.com>
2025-07-09 16:48:04 +0300

    goals: fix default value properties

--- src/main/java/school/faang/user_service/config/property/GoalProperty.java
+++ src/main/java/school/faang/user_service/config/property/GoalProperty.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @ConfigurationProperties(prefix = "goal")
-public record GoalProperty(@DefaultValue("10") int minParticipantsCount,
-                           @DefaultValue("20") int activeGoalsLimit) {
+public record GoalProperty(@DefaultValue("1") int minParticipantsCount,
+                           @DefaultValue("3") int activeGoalsLimit) {
 }
