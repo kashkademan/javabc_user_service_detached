14df6a51659ff0c135f40d106e7871802fddddd8
avzag1 <avzag@yandex.ru>
2025-07-09 23:10:56 +0300

    tests in progress

--- src/test/java/school/faang/user_service/controller/EducationControllerIntegrationTest.java
+++ src/test/java/school/faang/user_service/controller/EducationControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package school.faang.user_service.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +9,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import school.faang.user_service.dto.user.EducationDto;
-import school.faang.user_service.entity.user.Education;
-import school.faang.user_service.entity.user.User;
 import school.faang.user_service.service.education.EducationService;
 
 @SpringBootTest
@@ -26,12 +27,14 @@ public class EducationControllerIntegrationTest {
 
     @Test
     public void getEducationById_successResult() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
         EducationDto education = new EducationDto( 2000, 2005, "inst1",
                 "middle", "spec1");
         Mockito.when(educationService.getEducationById(1L)).thenReturn(education);
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/education?educationId=2"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.yearTo").value(2003));
-
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/education?educationId=1"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        byte[] contentAsByteArray = resultActions.andReturn().getResponse().getContentAsByteArray();
+        EducationDto result = mapper.readValue(contentAsByteArray, EducationDto.class);
+        Assertions.assertEquals(education.getEducationLevel(), result.getEducationLevel());
     }
 }
