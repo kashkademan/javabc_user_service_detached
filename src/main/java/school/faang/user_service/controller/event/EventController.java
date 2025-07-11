643d2c47618a51b1788d3bc33730a9fb98a5bf4c
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2025-07-11 22:54:00 +0200

    add REST endpoint mappings to EventController

--- src/main/java/school/faang/user_service/controller/event/EventController.java
+++ src/main/java/school/faang/user_service/controller/event/EventController.java
@@ -3,6 +3,13 @@ package school.faang.user_service.controller.event;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.event.CreateEventDto;
 import school.faang.user_service.dto.event.EventDto;
@@ -15,22 +22,27 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@RequestMapping("/api/v1/events")
 public class EventController {
     private final EventService eventService;
 
-    public EventDto create(@Valid CreateEventDto eventDto) {
+    @PostMapping
+    public EventDto create(@Valid @RequestBody CreateEventDto eventDto) {
         return eventService.create(eventDto);
     }
 
-    public EventDto update(long eventId, @Valid UpdateEventDto updateEventDto) {
+    @PutMapping("/{eventId}")
+    public EventDto update(@PathVariable long eventId, @Valid @RequestBody UpdateEventDto updateEventDto) {
         return eventService.update(eventId, updateEventDto);
     }
 
+    @GetMapping
     public List<EventDto> getByFilters(EventFilterDto filters) {
         return eventService.getByFilters(filters);
     }
 
-    public void delete(long eventId) {
+    @DeleteMapping("/{eventId}")
+    public void delete(@PathVariable long eventId) {
         eventService.delete(eventId);
     }
 }
