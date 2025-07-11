9d2bd3aa12a25c5926e6545326ed7e94c8050808
AlexandrPopov <164056516+Aleck210popov@users.noreply.github.com>
2025-07-11 19:13:56 +0300

    BJS2-84620 save commit

--- src/main/java/school/faang/user_service/repository/promotion/PromotionRedisRepository.java
+++ src/main/java/school/faang/user_service/repository/promotion/PromotionRedisRepository.java
@@ -7,10 +7,8 @@ import school.faang.user_service.model.redis.promotion.PromotionRedisModel;
 import java.util.Optional;
 
 @Repository
-public interface PromotionRedisRepository extends CrudRepository<PromotionRedisModel, String>
-        ,
-        PromotionRedisRepositoryNative
-{
+public interface PromotionRedisRepository extends CrudRepository<PromotionRedisModel, String>,
+        PromotionRedisRepositoryNative {
     Optional<PromotionRedisModel> findByEventId(Long eventId);
 
     Optional<PromotionRedisModel> findByUserId(Long userId);
