cf9e41998f084ad3627f96c5f2bbf6b59ae57a5e
Yulia <bualex1983@mail.ru>
2025-07-12 16:50:37 +0300

    addition

--- src/main/java/school/faang/user_service/education_addition/mapper/EducationMapper.java
+++ src/main/java/school/faang/user_service/education_addition/mapper/EducationMapper.java
@@ -1,10 +1,6 @@
 package school.faang.user_service.education_addition.mapper;
 
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.education_addition.EducationDto;
 import school.faang.user_service.entity.Education;
@@ -13,14 +9,7 @@ import school.faang.user_service.entity.User;
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EducationMapper {
 
-    @Mapping(target = "user", ignore = true)
     Education toEntity(EducationDto dto);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "user", source = "user")
     Education toEntity(EducationDto dto , User user );
-
-    void updateEntity(@MappingTarget Education education, EducationDto dto);
-
     EducationDto toDto(Education education);
 }
