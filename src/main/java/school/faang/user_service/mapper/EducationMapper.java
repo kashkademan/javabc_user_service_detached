187050a41d482de18bce080e6113ff78646ea5d5
Andrey Fomchenko <35478@mail.ru>
2025-07-17 23:49:59 +0500

    Test Education BJS2-84012 готовы к реаью

--- src/main/java/school/faang/user_service/mapper/EducationMapper.java
+++ src/main/java/school/faang/user_service/mapper/EducationMapper.java
@@ -3,6 +3,7 @@ package school.faang.user_service.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import school.faang.user_service.dto.education.UpdateEducationDto;
 import school.faang.user_service.dto.user.CreateEducationDto;
@@ -19,7 +20,8 @@ import school.faang.user_service.entity.user.User;
  * @author fomchenkoandrey
  */
 @Mapper(componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EducationMapper {
 
     /**
