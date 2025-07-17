c30f14266d3658674ba05092b2216005702d14bb
hohlovps95 <wiwisector@gmail.com>
2025-07-17 20:02:27 +0300

    + unit test

--- src/test/java/school/faang/user_service/service/skill/SkillServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/skill/SkillServiceImplTest.java
@@ -24,7 +24,11 @@ import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 
 @ExtendWith(MockitoExtension.class)
