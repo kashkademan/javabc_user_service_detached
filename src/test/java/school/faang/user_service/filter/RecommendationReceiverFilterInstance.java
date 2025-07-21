02c02297fb03d91bfbff0d3526bcd561fd0694f3
Ivi-Anastasia <qwwsoul@gmail.com>
2025-07-21 03:03:02 +0100

    Checkstyle correction

--- src/test/java/school/faang/user_service/filter/RecommendationReceiverFilterInstance.java
+++ src/test/java/school/faang/user_service/filter/RecommendationReceiverFilterInstance.java
@@ -5,7 +5,7 @@ import school.faang.user_service.entity.recommendation.Recommendation;
 
 import java.util.stream.Stream;
 
-public class RecommendationReceiverFilterInstance implements RecommendationFilter{
+public class RecommendationReceiverFilterInstance implements RecommendationFilter {
     @Override
     public boolean isApplicable(RecommendationFilterDto filter) {
         return true;
