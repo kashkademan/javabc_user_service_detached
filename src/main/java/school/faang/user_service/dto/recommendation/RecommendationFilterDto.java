de34e073d84e8963e8a9afdd0f1508068d3fa57a
hohlovps95 <wiwisector@gmail.com>
2025-07-22 23:59:08 +0300

    + Удаление фильтров 2

--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationFilterDto.java
+++
@@ -1,7 +0,0 @@
-package school.faang.user_service.dto.recommendation;
-
-import lombok.Builder;
-
-@Builder
-public record RecommendationFilterDto(String contentContains, Long authorId, Long receiverId) {
-}
