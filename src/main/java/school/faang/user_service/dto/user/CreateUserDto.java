69dd27e4f253e809e71565b179b23c70e8d7bc7e
RomanenkoViktor080 <138984372+RomanenkoViktor080@users.noreply.github.com>
2025-07-22 23:35:29 +0300

    Feature/bjs2 84704 spring security jwt (#3256)
    * BJS2-84423
    - CRUD по Goal
    - Создание FilterBuilder и фильтры для сущности Goal
    - Валидаторы|политики для операций над Goal
    - Создание DTOs, маппера для Goal
    
    FilterBuilder - для sql фильтрации сущностей в системе
    
    * Init swagger
    
    * Работа со скилами при создании/изменении целей
    
    * swagger: проставлены теги и скорректированы параметры в API‑документации
    
    * fixes (code style)
    
    * delete goal endpoint fix response body
    
    * fixes, goal get pageable, goal service tests, goal policies tests
    
    * integrate Spring Security and JWT, secure routes
    
    * integrate Spring Security and JWT, secure routes
    
    * refactoring, fixes
    
    * create and setup skills (#3217)
    
    * create and setup skills
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * Refactor mappers and add new mapping methods
    
    * add global exception handling
    
    * add SkillValidator and improve exception handling
    
    * add SkillValidator and improve exception handling
    
    * fixes
    
    * create and setup skills (#3217)
    
    * create and setup skills
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * Refactor mappers and add new mapping methods
    
    * add global exception handling
    
    * add SkillValidator and improve exception handling
    
    * add SkillValidator and improve exception handling
    
    * create and setup skills (#3217)
    
    * create and setup skills
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * fix: apply Checkstyle fixes in skill service setup
    
    * Refactor mappers and add new mapping methods
    
    * add global exception handling
    
    * add SkillValidator and improve exception handling
    
    * add SkillValidator and improve exception handling
    
    * merge
    
    * refresh tokens, refactoring
    
    * merge
    
    * AuthServiceImplTest, fixes
    
    * permit all endpoints
    
    ---------
    
    Co-authored-by: Maria Sakhno <69187205+mysosnovskaya@users.noreply.github.com>
    Co-authored-by: dzhelobkovich <100469424+dzhelobkovich@users.noreply.github.com>

--- src/main/java/school/faang/user_service/dto/user/CreateUserDto.java
+++ src/main/java/school/faang/user_service/dto/user/CreateUserDto.java
@@ -1,9 +1,25 @@
 package school.faang.user_service.dto.user;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateUserDto(
+        @NotBlank(message = "Введите username")
+        @Schema(description = "Username пользователя")
         String username,
+
+        @NotBlank(message = "Введите email")
+        @Email(message = "Некорректный email")
+        @Schema(description = "Email", example = "test@gmail.com")
         String email,
+
+        @NotBlank(message = "Введите пароль")
+        @Schema(description = "Пароль")
         String password,
+
+        @NotBlank(message = "Не определена страна пользователя")
+        @Schema(description = "Id страны пользователя", example = "1")
         Long countryId
 ) {
 }
