4d7b29a6ee9494ff70666863ee4a9ee1050f2e5c
hohlovps95 <wiwisector@gmail.com>
2025-07-22 23:52:34 +0300

    Откат к коммиту d1516b86f42a0268bbcc1d3f9e75f7cae0a2af7b

--- src/main/java/school/faang/user_service/dto/skill/SkillCandidateDto.java
+++ src/main/java/school/faang/user_service/dto/skill/SkillCandidateDto.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.dto.skill;
 
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
--- src/main/java/school/faang/user_service/service/skill/SkillServiceImpl.java
+++ src/main/java/school/faang/user_service/service/skill/SkillServiceImpl.java
@@ -1,7 +1,9 @@
 package school.faang.user_service.service.skill;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.skill.CreateSkillDto;
@@ -16,32 +18,40 @@ import school.faang.user_service.repository.user.SkillRepository;
 
 import java.util.List;
 
+@Slf4j
+@Setter
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class SkillServiceImpl implements SkillService {
     private final SkillRepository skillRepository;
     private final SkillMapper skillMapper;
     private final SkillOfferRepository skillOfferRepository;
+
+    @Value("${skill.minimal.offers}")
+    private int minimalSkillOffers;
 
     @Override
     @Transactional
     public SkillDto create(CreateSkillDto skillDto) {
         log.info("Получили новый объект по RestAPI: {} ", skillDto);
         Skill skill = skillMapper.toSkill(skillDto);
+
         log.info("Проверяем, нет ли уже такого заголовка в базе");
         if (skillRepository.existsByTitle(skill.getTitle())) {
-            throw new DataValidationException("Данный заголовок уже существует");
+            throw new DataValidationException("Данный заголовок уже существует: " + skill.getTitle());
         }
 
+        log.info("Начинаем писать в базу: {} ", skill);
         skill = skillRepository.save(skill);
         log.info("В базу сохранен новый объект: {} ", skill);
+
         return skillMapper.toSkillDto(skill);
     }
 
     @Override
     public List<SkillDto> getByUserId(Long userId) {
         List<Skill> skills = skillRepository.findAllByUserId(userId);
+        validateListNotEmpty(skills, "skill");
         if (skills.isEmpty()) {
             throw new EntityNotFoundException("No skills found for user with id " + userId);
         }
@@ -53,9 +63,7 @@ public class SkillServiceImpl implements SkillService {
     @Override
     public List<SkillCandidateDto> getOfferedSkills(long userId) {
         List<Skill> skills = skillRepository.findSkillsOfferedToUser(userId);
-        if (skills.isEmpty()) {
-            throw new EntityNotFoundException("No offered skills found for user with id " + userId);
-        }
+        validateListNotEmpty(skills, "skill");
         return skills.stream()
                 .map(skill -> skillMapper.toSkillDto(skill))
                 .map(skill -> {
@@ -69,14 +77,20 @@ public class SkillServiceImpl implements SkillService {
     @Transactional
     public void acquireSkillFromOffers(long skillId, long userId) {
         log.info("Пользователь с id {} хочет приобрести скилл с id {}", userId, skillId);
-        if (!skillRepository.existsById(skillId)) {
+        if (skillRepository.existsById(skillId)) {
             throw new EntityNotFoundException("Skill with id " + skillId + " not found");
         }
-        if (skillOfferRepository.countAllOffersOfSkill(skillId, userId) >= 3) {
+        if (skillOfferRepository.countAllOffersOfSkill(skillId, userId) >= minimalSkillOffers) {
+            log.info("Добавляем скилл с id {} пользователю с id {}", skillId, userId);
             skillRepository.assignSkillToUser(skillId, userId);
         } else {
             throw new IllegalStateException("Требуется не менее трех предложений скилла для его приобретения");
         }
+    }
 
+    private void validateListNotEmpty(List<Skill> value, String paramName) {
+        if (value.isEmpty()) {
+            throw new DataValidationException(paramName + " лист объектов не найден");
+        }
     }
 }
