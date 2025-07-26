2603e5395be3ef40415cd284799be7e2239f3524
Askar <asklooka@gmail.com>
2025-07-26 10:54:55 -0400

    ваппавыфцукегшщзххэжбьтимсирн

--- src/main/java/school/faang/user_service/service/goal/GoalServiceImpl.java
+++ src/main/java/school/faang/user_service/service/goal/GoalServiceImpl.java
@@ -6,6 +6,7 @@ import school.faang.user_service.dto.goal.CreateGoalDto;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.UpdateGoalDto;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.entity.user.User;
 import school.faang.user_service.mapper.GoalMapper;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.repository.user.UserRepository;
@@ -19,6 +20,11 @@ public class GoalServiceImpl implements GoalService {
 
     @Override
     public GoalDto create(CreateGoalDto createGoalDto) {
+        User user = userRepository.getByIdOrThrow(userContext.getUserId());
+        if (user.getGoals().size() > 1) {
+            throw new
+        }
+
         Goal goal = goalMapper.toGoal(createGoalDto);
         goal.setMentor(userRepository.getByIdOrThrow(createGoalDto.mentorId()));
         goal.setUsers(userRepository.findAll());
