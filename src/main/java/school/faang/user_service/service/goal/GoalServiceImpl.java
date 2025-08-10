e2ac6ee630631cf229854c8b8bd1640be1a449b8
Askar <asklooka@gmail.com>
2025-08-10 13:09:25 -0400

    kjfkfljafad

--- src/main/java/school/faang/user_service/service/goal/GoalServiceImpl.java
+++ src/main/java/school/faang/user_service/service/goal/GoalServiceImpl.java
@@ -6,11 +6,16 @@ import school.faang.user_service.dto.goal.CreateGoalDto;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.dto.goal.UpdateGoalDto;
 import school.faang.user_service.entity.goal.Goal;
+import school.faang.user_service.entity.goal.GoalStatus;
 import school.faang.user_service.entity.user.User;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.mapper.GoalMapper;
 import school.faang.user_service.repository.goal.GoalRepository;
 import school.faang.user_service.repository.user.UserRepository;
+
+import java.util.List;
+
+import static school.faang.user_service.entity.goal.GoalStatus.ACTIVE;
 
 @RequiredArgsConstructor
 public class GoalServiceImpl implements GoalService {
@@ -22,7 +27,14 @@ public class GoalServiceImpl implements GoalService {
     @Override
     public GoalDto create(CreateGoalDto createGoalDto) {
         User user = userRepository.getByIdOrThrow(userContext.getUserId());
-        if (user.getGoals().size() > 1) {
+
+        if (!user.getMentees().containsAll(createGoalDto.userIds())
+                && createGoalDto.userIds().contains(createGoalDto.mentorId())) {
+            throw new DataValidationException("создать цель может либо ментор для своих менти, " +
+                    "либо пользователь сам для себя");
+        }
+
+        if (!hasNoMoreThanTwoActiveGoals(user)) {
             throw new DataValidationException("Слишком много активных целей!");
         }
 
@@ -30,6 +42,16 @@ public class GoalServiceImpl implements GoalService {
         goal.setMentor(userRepository.getByIdOrThrow(createGoalDto.mentorId()));
         goal.setUsers(userRepository.findAll());
     }
+
+
+    private boolean hasNoMoreThanTwoActiveGoals(User user) {
+        long activeCount = user.getGoals().stream()
+                .filter(goal -> goal.getStatus() == GoalStatus.ACTIVE)
+                .limit(3)
+                .count();
+        return activeCount <= 2;
+    }
+
 
     @Override
     public GoalDto update(long goalId, UpdateGoalDto updateGoalDto) {
