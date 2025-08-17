9b58cd13ed47853c9da34b89a8239be00ea4d90a
mrnght <misterday116@gmail.com>
2025-08-17 21:03:32 +0800

    BJS2-84147 исправил чекстайл

--- src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
@@ -2,7 +2,6 @@ package school.faang.user_service.publisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.retry.annotation.Backoff;
