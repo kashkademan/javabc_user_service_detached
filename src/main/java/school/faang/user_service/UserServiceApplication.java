4e6af68ae2f379d151fbcdad6b98828eb11f3289
mrnght <misterday116@gmail.com>
2025-08-17 19:05:04 +0800

    BJS2-84147 сделал публикацию события в редис асинхронным

--- src/main/java/school/faang/user_service/UserServiceApplication.java
+++ src/main/java/school/faang/user_service/UserServiceApplication.java
@@ -3,11 +3,13 @@ package school.faang.user_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients("school.faang.user_service.client")
 @EnableScheduling
+@EnableRetry
 public class UserServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/EventStartEventPublisher.java
@@ -2,8 +2,12 @@ package school.faang.user_service.publisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.EventStartEvent;
 
@@ -16,6 +20,8 @@ public class EventStartEventPublisher {
     @Value("${spring.data.redis.channel.event}")
     private String topicName;
 
+    @Async
+    @Retryable(retryFor = {Exception.class}, maxAttempts = 3, backoff = @Backoff(delay = 1000, multiplier = 2))
     public void publish(EventStartEvent event) {
         redisTemplate.convertAndSend(topicName, event);
     }
