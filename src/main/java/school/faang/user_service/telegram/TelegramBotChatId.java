d23a9fa801cd7ca80c4299014bccdd6f3b4473b5
JekaCAP <agentgeka@yandex.ru>
2025-08-22 02:02:13 +0500

    BJS2_84156 Общие правки 1.1

--- src/main/java/school/faang/user_service/telegram/TelegramBotChatId.java
+++ src/main/java/school/faang/user_service/telegram/TelegramBotChatId.java
@@ -8,9 +8,6 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import school.faang.user_service.config.context.UserContext;
-import school.faang.user_service.service.user.UserService;
 
 @Component
 @Slf4j
