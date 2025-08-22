d15ae10f67725c4f355e116dfa0304c1c6b0080a
JekaCAP <agentgeka@yandex.ru>
2025-08-22 20:33:23 +0500

    BJS2_84156 Профиль

--- src/main/java/school/faang/user_service/telegram/TelegramBotInitializer.java
+++ src/main/java/school/faang/user_service/telegram/TelegramBotInitializer.java
@@ -2,12 +2,14 @@ package school.faang.user_service.telegram;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Service
+@Profile("!test")
 @RequiredArgsConstructor
 public class TelegramBotInitializer {
 
