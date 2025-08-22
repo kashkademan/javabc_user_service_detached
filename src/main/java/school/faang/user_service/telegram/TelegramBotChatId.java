5bd6029de4d0b5e78ccbbd78d5f9e6d22474e652
JekaCAP <agentgeka@yandex.ru>
2025-08-22 15:13:01 +0500

    BJS2_84156 Время теста

--- src/main/java/school/faang/user_service/telegram/TelegramBotChatId.java
+++ src/main/java/school/faang/user_service/telegram/TelegramBotChatId.java
@@ -8,13 +8,15 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import school.faang.user_service.service.user.UserService;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class TelegramBotChatId extends TelegramLongPollingBot {
 
-    private final StartCommand startCommand;
+    private final UserService userService;
 
     @Value("${telegram.bot-username}")
     private String botUsername;
@@ -42,9 +44,30 @@ public class TelegramBotChatId extends TelegramLongPollingBot {
             String text = message.getText();
 
             if (text.startsWith("/start")) {
-                String payload = text.replace("/start", "").trim();
-                startCommand.handle(chatId, payload);
+                handleStartCommand(chatId, text);
             }
+        }
+    }
+
+    private void handleStartCommand(Long chatId, String text) {
+        String payload = text.replace("/start", "").trim();
+
+        try {
+            Long userId = Long.parseLong(payload);
+            userService.linkChatId(userId, chatId);
+
+            sendMessage(chatId, "Ваш Telegram успешно привязан к аккаунту!");
+        } catch (NumberFormatException e) {
+            sendMessage(chatId, "Некорректный формат userId!");
+        }
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        try {
+            SendMessage sendMessage = new SendMessage(chatId.toString(), text);
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Ошибка при отправке сообщения", e);
         }
     }
 }
