3559fe7f75e14e16f1653fae5fd7faf4829a02ea
Tien-PhanT <112714509+phtanya@users.noreply.github.com>
2025-08-31 17:47:45 +0200

    BJS2-83806

--- src/main/java/school/faang/user_service/publisher/GoalCompletedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/GoalCompletedEventPublisher.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.publisher;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
