e0e80846aaed773de04175d788c06447571b8f2f
mrnght <misterday116@gmail.com>
2025-09-05 12:12:25 +0800

    BJS2-84160 пофиксил чекстайл

--- src/main/java/school/faang/user_service/publisher/FollowerEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/FollowerEventPublisher.java
@@ -2,7 +2,6 @@ package school.faang.user_service.publisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.retry.annotation.Backoff;
