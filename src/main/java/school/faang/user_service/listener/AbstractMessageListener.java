31a83a471f4c3368eacd5a74dacf05e9881929a5
Linempy <mychinnik@yandex.ru>
2025-09-11 18:28:41 +0300

    BJS2-84236-leaderboard - поправил checkstyle

--- src/main/java/school/faang/user_service/listener/AbstractMessageListener.java
+++ src/main/java/school/faang/user_service/listener/AbstractMessageListener.java
@@ -2,7 +2,6 @@ package school.faang.user_service.listener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 
 /**
  * Общая логика слушателя ивентов
