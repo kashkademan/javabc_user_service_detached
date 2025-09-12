e66994e94e7f80194c67d7dfa009270e118ab22a
Askar <asklooka@gmail.com>
2025-09-12 13:11:14 -0400

    BJS2-83600

--- src/main/java/school/faang/user_service/controller/goal/GoalController.java
+++ src/main/java/school/faang/user_service/controller/goal/GoalController.java
@@ -1,9 +1,13 @@
 package school.faang.user_service.controller.goal;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.goal.CreateGoalDto;
 import school.faang.user_service.dto.goal.GoalDto;
 import school.faang.user_service.service.goal.GoalService;
 
+@RestController
+@RequestMapping("/v1/")
 public class GoalController {
     private GoalService goalService;
 
