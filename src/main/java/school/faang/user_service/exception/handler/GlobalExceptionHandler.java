f01fe327c861c8ddb734d582eedf333c7f611342
Volodymyr <dqkvii@gmail.com>
2025-09-28 20:20:42 +0200

    fix-style

--- src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/school/faang/user_service/exception/handler/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package school.faang.user_service.exception.handler;
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
