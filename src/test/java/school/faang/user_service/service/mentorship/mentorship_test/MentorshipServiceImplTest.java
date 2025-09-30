1ed642402aecaad9c2ac4ac39d610188e4ba4830
Bekhter Roman <curses93@mail.ru>
2025-09-30 19:30:47 +0300

    BJS2-96444 mentorship done

--- src/test/java/school/faang/user_service/service/mentorship/mentorship_test/MentorshipServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/mentorship/mentorship_test/MentorshipServiceImplTest.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.service.mentorship.mentorship_service_test;
+package school.faang.user_service.service.mentorship.mentorship_test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
