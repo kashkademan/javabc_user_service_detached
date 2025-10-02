239962a9a4b60e7d0f695d119508dc8292631af3
Kirill Mir <loocyep@gmail.com>
2025-10-03 00:03:05 +0300

    BJS2-96030: Рабочий график -добавил класс валидатор бизнес-логики, использовал в сервисе -добавил аннотации для валидации в дто -добавил контроллер с методом добавления рабочего графика пользователю

---
+++ src/main/java/school/faang/user_service/controller/workschedule/WorkScheduleController.java
@@ -0,0 +1,18 @@
+package school.faang.user_service.controller.workschedule;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import school.faang.user_service.config.context.UserContext;
+import school.faang.user_service.dto.workschedule.WorkScheduleDto;
+import school.faang.user_service.service.workschedule.WorkScheduleService;
+
+@Controller
+@RequiredArgsConstructor
+public class WorkScheduleController {
+    private final WorkScheduleService workScheduleService;
+    private final UserContext userContext;
+
+    public WorkScheduleDto addWorkSchedule(WorkScheduleDto workScheduleDto) {
+        return workScheduleService.addWorkSchedule(userContext.getUserId(), workScheduleDto);
+    }
+}
---
+++ src/main/java/school/faang/user_service/dto/workschedule/WorkScheduleValidator.java
@@ -0,0 +1,19 @@
+package school.faang.user_service.dto.workschedule;
+
+import org.springframework.stereotype.Component;
+import school.faang.user_service.exception.DataValidationException;
+
+@Component
+public class WorkScheduleValidator {
+    public void validate(WorkScheduleDto dto) {
+        if (!dto.startTime().isBefore(dto.startLunch())) {
+            throw new DataValidationException("Стартовое время графика должно быть раньше времени старта обеда");
+        }
+        if (!dto.startLunch().isBefore(dto.endLunch())) {
+            throw new DataValidationException("Время старта обеда должно быть раньше времени его окончания");
+        }
+        if (!dto.endLunch().isBefore(dto.endTime())) {
+            throw new DataValidationException("Время конца обеда должно быть раньше времени конца графика");
+        }
+    }
+}
