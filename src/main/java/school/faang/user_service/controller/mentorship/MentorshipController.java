ca2099322474246d58cf2ef0fdb0d55ea6c2f18a
Kuanysh <skn_88@mail.ru>
2025-10-02 23:44:14 +0500

    feature-BJS2-96441-become-a-mentor

---
+++ src/main/java/school/faang/user_service/controller/mentorship/MentorshipController.java
@@ -0,0 +1,41 @@
+package school.faang.user_service.controller.mentorship;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.service.mentorship.MentorshipService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mentorships")
+@RequiredArgsConstructor
+public class MentorshipController {
+    private final MentorshipService mentorshipService;
+
+    @PostMapping("/{mentorId}/{menteeId}")
+    void addMentorship(@PathVariable long mentorId, @PathVariable long menteeId) {
+        mentorshipService.addMentorship(mentorId, menteeId);
+    }
+
+    @GetMapping("/{userId}/mentees")
+    List<UserDto> getMentees(@PathVariable long userId) {
+        return mentorshipService.getMentees(userId);
+    }
+
+    @GetMapping("/{userId}/mentors")
+    List<UserDto> getMentors(@PathVariable long userId) {
+        return mentorshipService.getMentors(userId);
+    }
+
+    @DeleteMapping("/{menteeId}/{mentorId}")
+    void deleteMentorship(@PathVariable long menteeId, @PathVariable long mentorId) {
+        mentorshipService.deleteMentorship(menteeId, mentorId);
+    }
+
+}
---
+++ src/main/java/school/faang/user_service/service/mentorship/MentorshipServiceImpl.java
@@ -0,0 +1,86 @@
+package school.faang.user_service.service.mentorship;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.config.context.UserContext;
+import school.faang.user_service.dto.user.UserDto;
+import school.faang.user_service.entity.user.User;
+import school.faang.user_service.exception.DataValidationException;
+import school.faang.user_service.exception.ForbiddenException;
+import school.faang.user_service.mapper.UserMapper;
+import school.faang.user_service.repository.mentorship.MentorshipRepository;
+import school.faang.user_service.validation.mentorship.MentorshipValidation;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MentorshipServiceImpl implements MentorshipService {
+    private final MentorshipRepository mentorshipRepository;
+    private final MentorshipValidation mentorshipValidation;
+    private final UserMapper userMapper;
+    private final UserContext userContext;
+
+    @Override
+    public void addMentorship(long mentorId, long menteeId) {
+        if (!mentorshipValidation.canAddMentorship(mentorId,
+                menteeId,
+                (mentor, mentee) -> mentor != mentee)) {
+            throw new DataValidationException("Mentor Id and Mentee id can't be equal");
+        }
+
+        User mentor = mentorshipRepository.getByIdOrThrow(mentorId);
+        User mentee = mentorshipRepository.getByIdOrThrow(menteeId);
+
+        if (mentee.getMentors().contains(mentor)) {
+            log.info("Mentee {} already has mentor {}", menteeId, mentorId);
+            return;
+        }
+
+        mentee.getMentors().add(mentor);
+        mentorshipRepository.save(mentee);
+
+        log.info("Mentorship created between mentor {} and mentee {}", mentorId, menteeId);
+    }
+
+    @Override
+    public List<UserDto> getMentees(long userId) {
+        User mentor = mentorshipRepository.getByIdOrThrow(userId);
+        return Optional.ofNullable(mentor.getMentees())
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(userMapper::toUserDto)
+                .toList();
+
+    }
+
+    @Override
+    public List<UserDto> getMentors(long userId) {
+        User mentee = mentorshipRepository.getByIdOrThrow(userId);
+        return Optional.ofNullable(mentee.getMentors())
+                .orElse(Collections.emptyList())
+                .stream()
+                .map(userMapper::toUserDto)
+                .toList();
+    }
+
+    @Override
+    public void deleteMentorship(long menteeId, long mentorId) {
+        long currentUserId = userContext.getUserId();
+
+        if (currentUserId != menteeId && currentUserId != mentorId) {
+            throw new ForbiddenException("You are not a participant in the deleting mentorship!");
+        }
+
+        User mentor = mentorshipRepository.getByIdOrThrow(mentorId);
+        User mentee = mentorshipRepository.getByIdOrThrow(menteeId);
+        mentee.getMentors().remove(mentor);
+
+        mentorshipRepository.save(mentee);
+    }
+
+}
