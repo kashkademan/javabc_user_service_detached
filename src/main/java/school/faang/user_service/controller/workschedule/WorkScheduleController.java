e1e75dc74a3a6e69f41998558669f42c05662114
Kirill Mir <loocyep@gmail.com>
2025-10-06 22:46:05 +0300

    BJS2-96030: -implement REST endpoints for one WorkSchedule method

--- src/main/java/school/faang/user_service/controller/workschedule/WorkScheduleController.java
+++ src/main/java/school/faang/user_service/controller/workschedule/WorkScheduleController.java
@@ -1,13 +1,17 @@
 package school.faang.user_service.controller.workschedule;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.workschedule.UpdateWorkScheduleDto;
 import school.faang.user_service.dto.workschedule.WorkScheduleDto;
 import school.faang.user_service.service.workschedule.WorkScheduleService;
 
-@Controller
+@RequestMapping("/work-schedules")
+@RestController
 @RequiredArgsConstructor
 public class WorkScheduleController {
     private final WorkScheduleService workScheduleService;
@@ -21,7 +25,8 @@ public class WorkScheduleController {
         return workScheduleService.updateWorkSchedule(userContext.getUserId(), workScheduleId, updateWorkScheduleDto);
     }
 
-    public WorkScheduleDto getById(long workScheduleId) {
+    @GetMapping("/{id}")
+    public WorkScheduleDto getById(@PathVariable("id") long workScheduleId) {
         return workScheduleService.getById(workScheduleId);
     }
 }
