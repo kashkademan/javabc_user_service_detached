45aaf57b8246cfb1bb204b00f2416c38e2149834
Deathroels <loocyep@gmail.com>
2025-10-07 09:08:30 +0300

    BJS2-96030: Рабочий график -убрал ненужную валилацию в дто(проверка на уровне сущности, проверка имела бы смысл при приеме данных)

--- src/main/java/school/faang/user_service/dto/workschedule/UpdateWorkScheduleDto.java
+++ src/main/java/school/faang/user_service/dto/workschedule/UpdateWorkScheduleDto.java
@@ -1,16 +1,10 @@
 package school.faang.user_service.dto.workschedule;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.time.LocalTime;
 
-public record UpdateWorkScheduleDto(@NotNull(message = "Start time is required") LocalTime startTime,
-
-                                    @NotNull(message = "End time is required") LocalTime endTime,
-
-                                    @NotNull(message = "Lunch start time is required") LocalTime startLunch,
-
-                                    @NotNull(message = "Lunch end time is required") LocalTime endLunch,
-
-                                    @NotNull(message = "Timezone is required") String timezone) {
+public record UpdateWorkScheduleDto(LocalTime startTime,
+                                    LocalTime endTime,
+                                    LocalTime startLunch,
+                                    LocalTime endLunch,
+                                    String timezone) {
 }
--- src/main/java/school/faang/user_service/dto/workschedule/WorkScheduleDto.java
+++ src/main/java/school/faang/user_service/dto/workschedule/WorkScheduleDto.java
@@ -1,18 +1,11 @@
 package school.faang.user_service.dto.workschedule;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.time.LocalTime;
 
 public record WorkScheduleDto(long id,
-
-                              @NotNull(message = "Start time is required") LocalTime startTime,
-
-                              @NotNull(message = "End time is required") LocalTime endTime,
-
-                              @NotNull(message = "Lunch start time is required") LocalTime startLunch,
-
-                              @NotNull(message = "Lunch end time is required") LocalTime endLunch,
-
-                              @NotNull(message = "Timezone is required") String timezone) {
+                              LocalTime startTime,
+                              LocalTime endTime,
+                              LocalTime startLunch,
+                              LocalTime endLunch,
+                              String timezone) {
 }
