62204125354d9cc9a37bc9e700ba041f28f4f6d3
Deathroels <loocyep@gmail.com>
2025-10-07 12:46:37 +0300

    BJS2-96030: Рабочий график -добавил оставшиеся REST endpoints, для контроллера

--- src/main/java/school/faang/user_service/controller/workschedule/WorkScheduleController.java
+++ src/main/java/school/faang/user_service/controller/workschedule/WorkScheduleController.java
@@ -3,6 +3,9 @@ package school.faang.user_service.controller.workschedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.config.context.UserContext;
@@ -17,11 +20,13 @@ public class WorkScheduleController {
     private final WorkScheduleService workScheduleService;
     private final UserContext userContext;
 
-    public WorkScheduleDto addWorkSchedule(WorkScheduleDto workScheduleDto) {
+    @PostMapping
+    public WorkScheduleDto addWorkSchedule(@RequestBody WorkScheduleDto workScheduleDto) {
         return workScheduleService.addWorkSchedule(userContext.getUserId(), workScheduleDto);
     }
 
-    public WorkScheduleDto updateWorkSchedule(long workScheduleId, UpdateWorkScheduleDto updateWorkScheduleDto) {
+    @PutMapping("/{id}")
+    public WorkScheduleDto updateWorkSchedule(@PathVariable("id") long workScheduleId, @RequestBody UpdateWorkScheduleDto updateWorkScheduleDto) {
         return workScheduleService.updateWorkSchedule(userContext.getUserId(), workScheduleId, updateWorkScheduleDto);
     }
 
