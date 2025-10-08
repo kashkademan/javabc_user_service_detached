3e8491f6b3fce41422dc2180931286a9e482ad56
Bekhter Roman <curses93@mail.ru>
2025-10-08 17:36:56 +0300

    BJS2-96620 Fix failing test

--- src/test/java/school/faang/user_service/pipeline_test/TestPipelineFail.java
+++ src/test/java/school/faang/user_service/pipeline_test/TestPipelineFail.java
@@ -1,12 +1,12 @@
 package school.faang.user_service.pipeline_test;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestPipelineFail {
 
     @Test
-    void shouldFail() {
-        fail("This test should fail to demonstrate CI pipeline");
+    void shouldPass() {
+        assertTrue(true, "This test should pass now");
     }
 }
