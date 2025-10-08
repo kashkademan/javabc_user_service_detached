21b26cbbf3825f7c1beef615a72c146dc0618214
Bekhter Roman <curses93@mail.ru>
2025-10-08 17:28:05 +0300

    BJS2-96620 Add failing test for pipeline

---
+++ src/test/java/school/faang/user_service/pipeline_test/TestPipelineFail.java
@@ -0,0 +1,12 @@
+package school.faang.user_service.pipeline_test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TestPipelineFail {
+
+    @Test
+    void shouldFail() {
+        fail("This test should fail to demonstrate CI pipeline");
+    }
+}
