340fd796ada7fda111c7d7b00233aa5c56f25ae3
Deathroels <loocyep@gmail.com>
2025-10-09 10:59:29 +0300

    BJS2-96030: Рабочий график -исправлено поведение мапера (при обновлении, текущие поля в сущности, не меняются на поля переданного дто, если там значение null)

--- src/main/java/school/faang/user_service/mapper/WorkScheduleMapper.java
+++ src/main/java/school/faang/user_service/mapper/WorkScheduleMapper.java
@@ -2,11 +2,12 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import school.faang.user_service.dto.workschedule.UpdateWorkScheduleDto;
 import school.faang.user_service.dto.workschedule.WorkScheduleDto;
 import school.faang.user_service.entity.user.WorkSchedule;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface WorkScheduleMapper {
     WorkSchedule toWorkSchedule(WorkScheduleDto workScheduleDto);
 
