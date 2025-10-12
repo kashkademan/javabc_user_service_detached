564a1e95810bc3bf5ed33dee147bb40bcf0c40e0
Pavel-Lukyanov <PavelLukyanov2017@yandex.ru>
2025-10-12 16:09:15 +0500

    Правки чекстайла

--- src/test/java/school/faang/user_service/service/skill/SkillServiceImplTest.java
+++ src/test/java/school/faang/user_service/service/skill/SkillServiceImplTest.java
@@ -30,8 +30,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.argThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SkillServiceImplTest {
