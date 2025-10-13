507352ff378dcc9415527604a765366b006eb232
p.dmitrenko <mo9keylust@mail.ru>
2025-10-13 16:34:22 +0300

    BJS2-96233 // In progress

--- src/main/java/school/faang/user_service/dto/career/UpdateCareerDto.java
+++ src/main/java/school/faang/user_service/dto/career/UpdateCareerDto.java
@@ -1,12 +1,15 @@
 package school.faang.user_service.dto.career;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record UpdateCareerDto(LocalDate from,
+public record UpdateCareerDto(@NotNull LocalDate from,
                               LocalDate to,
-                              String company,
-                              String position) implements CareerDateDto {
+                              @NotBlank String company,
+                              @NotBlank String position)
+        implements CareerDateDto {
 }
