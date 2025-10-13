6017a750f44775d40fdd3c3550f529e61894cdd9
Deathroels <loocyep@gmail.com>
2025-10-13 10:59:53 +0300

    BJS2-96035: -убрал все метода из маппера кроме toWorkScheduleDto

--- src/main/java/school/faang/user_service/mapper/WorkScheduleMapper.java
+++ src/main/java/school/faang/user_service/mapper/WorkScheduleMapper.java
@@ -2,15 +2,10 @@ package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValuePropertyMappingStrategy;
-import school.faang.user_service.dto.workschedule.CreateWorkScheduleDto;
 import school.faang.user_service.dto.workschedule.WorkScheduleDto;
 import school.faang.user_service.entity.user.WorkSchedule;
 
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface WorkScheduleMapper {
-    WorkSchedule toCreateWorkSchedule(CreateWorkScheduleDto createWorkScheduleDto);
-
-    WorkSchedule toWorkSchedule(WorkScheduleDto workScheduleDto);
-
     WorkScheduleDto toWorkScheduleDto(WorkSchedule workSchedule);
 }
