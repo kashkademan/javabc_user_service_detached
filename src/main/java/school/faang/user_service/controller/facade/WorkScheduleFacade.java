8a4dd7ec7e0ab135e236be6efb226b0bad713460
Deathroels <loocyep@gmail.com>
2025-10-13 11:07:47 +0300

    BJS2-96035: -убрал log.info не имеющие смысла

--- src/main/java/school/faang/user_service/controller/facade/WorkScheduleFacade.java
+++ src/main/java/school/faang/user_service/controller/facade/WorkScheduleFacade.java
@@ -20,14 +20,12 @@ public class WorkScheduleFacade {
     public WorkScheduleDto addWorkSchedule(CreateWorkScheduleDto createWorkScheduleDto) {
         WorkSchedule workSchedule = workScheduleMapper.toCreateWorkSchedule(createWorkScheduleDto);
         WorkSchedule savedWorkSchedule = workScheduleService.addWorkSchedule(workSchedule);
-        log.info("WorkSchedule for user added");
 
         return workScheduleMapper.toWorkScheduleDto(savedWorkSchedule);
     }
 
     public WorkScheduleDto updateWorkSchedule(long workScheduleId, UpdateWorkScheduleDto updateWorkScheduleDto) {
         WorkSchedule workSchedule = workScheduleService.updateWorkSchedule(workScheduleId, updateWorkScheduleDto);
-        log.info("WorkSchedule was updated");
 
         return workScheduleMapper.toWorkScheduleDto(workSchedule);
     }
