1119878dc3db5c9bffa6d91a39b4711318f9bc78
Elya Khamzina <ekkhamzi@mail.ru>
2025-10-21 13:06:08 +0300

    checkstyle

--- src/main/java/school/faang/user_service/controller/user/UserSubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/user/UserSubscriptionController.java
@@ -76,7 +76,8 @@ public class UserSubscriptionController {
     }
 
     @GetMapping("/{followeeId}/followers")
-    @Operation(summary = "Get followers list", description = "Returns list of followers with filtering options", responses = {
+    @Operation(summary = "Get followers list", description = "Returns list of followers with filtering options",
+            responses = {
             @ApiResponse(responseCode = "200", description = "Followers list returned successfully"),
             @ApiResponse(responseCode = "404", description = "User not found")
     })
@@ -91,7 +92,8 @@ public class UserSubscriptionController {
     }
 
     @GetMapping("/{followerId}/followees")
-    @Operation(summary = "Get followees list", description = "Returns list of followees with filtering options", responses = {
+    @Operation(summary = "Get followees list", description = "Returns list of followees with filtering options",
+            responses = {
             @ApiResponse(responseCode = "200", description = "Followees list returned successfully"),
             @ApiResponse(responseCode = "404", description = "User not found")
     })
