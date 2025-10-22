76ef2c9cb22455e4cc86277b7b11b3a312954423
Andre` <acmisatyuk@gmail.com>
2025-10-22 23:52:55 +0800

    BJS2-96634: Система постов: User Service refactoring, added controller and Tests: initial version

--- src/main/java/school/faang/user_service/dto/user/CreateUserDto.java
+++ src/main/java/school/faang/user_service/dto/user/CreateUserDto.java
@@ -1,9 +1,23 @@
 package school.faang.user_service.dto.user;
 
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+
+
+@Builder
 public record CreateUserDto(
+        @NotBlank
+        @Size(min = 1, max = 50)
+        @Pattern(regexp = "^[A-Za-z0-9._-]+$", message = "Username may contain letters, digits, dot, underscore, hyphen")
         String username,
+
+        @NotBlank @Email
         String email,
+
+        @NotBlank
+        @Size(min = 8, max = 200)
         String password,
+
+        @NotNull @Positive
         Long countryId
-) {
-}
+) {}
--- src/main/java/school/faang/user_service/dto/user/UpdateUserDto.java
+++ src/main/java/school/faang/user_service/dto/user/UpdateUserDto.java
@@ -1,11 +1,26 @@
 package school.faang.user_service.dto.user;
 
+import lombok.Builder;
+import jakarta.validation.constraints.*;
+
+@Builder
 public record UpdateUserDto(
+        @Size(min = 1, max = 50)
+        @Pattern(regexp = "^[A-Za-z0-9._-]+$", message = "Username may contain letters, digits, dot, underscore, hyphen")
         String username,
+
+        @Email
         String email,
+
+        @Size(max = 30)
         String phone,
+
+        @Size(max = 500)
         String aboutMe,
+
+        @Positive
         Long countryId,
+
+        @Size(max = 100)
         String city
-) {
-}
+) {}
