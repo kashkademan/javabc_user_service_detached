3c7aee1d5c763134099dea40d112b2e91a1dd461
IyaLobach <lobach.iya@gmail.com>
2025-10-23 19:54:39 +0300

    fix: fixes after crossReview

--- src/main/java/school/faang/user_service/dto/recommendation/CreateRecommendationRequestDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/CreateRecommendationRequestDto.java
@@ -1,7 +1,6 @@
 package school.faang.user_service.dto.recommendation;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
