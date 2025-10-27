3d6c0137f3e2dad175d5b1b2190273b58c9aebe8
Viks <viktor.naroznoi@gmail.com>
2025-10-27 11:42:15 +0200

    --BJS2-96534 - RecommendationServiceImplTest done

--- src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
+++ src/main/java/school/faang/user_service/mapper/RecommendationMapper.java
@@ -1,6 +1,7 @@
 package school.faang.user_service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
 import school.faang.user_service.dto.recommendation.UpdateRecommendationDto;
@@ -9,6 +10,8 @@ import school.faang.user_service.entity.recommendation.Recommendation;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE)
 public interface RecommendationMapper {
 
+    @Mapping(source = "author.id", target = "authorId")
+    @Mapping(source = "receiver.id", target = "receiverId")
     RecommendationDto toRecommendationDto(Recommendation recommendation);
 
     void update(UpdateRecommendationDto updateRecommendationDto, @MappingTarget Recommendation entity);
