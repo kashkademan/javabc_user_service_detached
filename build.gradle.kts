6a84bc626986806ef658277ec055a3c446ea616b
Scherbi <shherbakov99ilya@mail.ru>
2025-10-28 18:57:18 +0300

    BJS2-96082 FIX IMPLEMENTIONS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,7 +66,6 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:postgresql")
     testImplementation("com.redis.testcontainers:testcontainers-redis-junit-jupiter:1.4.6")
-    testImplementation("org.testcontainers:minio:1.19.5")
 
     /**
      * Tests
