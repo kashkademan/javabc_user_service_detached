9dc32f1fdce03f860fb98ffc78c4fd482cd50752
Denis Chudov <Chdnru@gmail.com>
2025-11-02 18:59:58 +0300

    Fix-BJS2-96506-tests-request-for-mentorship.Добавил метод getUsersByIds в UserService

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,10 +1,9 @@
 package school.faang.user_service.controller.user;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.dto.user.CreateUserDto;
 import school.faang.user_service.dto.user.UpdateUserDto;
@@ -20,6 +20,7 @@ import school.faang.user_service.service.user.UserService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -28,15 +29,12 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserDto> getUsersByIds(
-            @RequestBody
-            @NotEmpty(message = "IDs list must not be empty")
-            List<@NotNull(message = "ID must not be null")
-            @Positive(message = "ID must be positive") Long> ids) {
-
-        return ids.stream()
-                .map(userService::getById)
-                .toList();
+    public List<UserDto> getUsersByIds(@RequestParam("ids") List<Long> ids) {
+        if (ids.isEmpty()) {
+            log.info("No userIds resolved from likes -> returning empty list");
+            return List.of();
+        }
+        return userService.getUsersByIds(ids);
     }
 
     @PostMapping
@@ -45,8 +43,7 @@ public class UserController {
     }
 
     @PutMapping("/{userId}")
-    public UserDto update(@PathVariable @Positive long userId,
-                          @RequestBody @Valid UpdateUserDto userDto) {
+    public UserDto update(@PathVariable @Positive long userId, @RequestBody @Valid UpdateUserDto userDto) {
         return userService.update(userId, userDto);
     }
 
