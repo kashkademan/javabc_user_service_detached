06cc1d36cf9e88a3dfbaa98828c083e3675a0a59
7glebalyot <7glebpavlovskij7@gmail.com>
2025-11-02 17:09:56 +0100

    Реализовал создание, удаление и обновление пользователей (#3472)
    * Реализовал создание, удаление и обновление пользователей
    
    * мелкий fix
    
    * Правка синтаксиса
    
    * Добавил описание в UserService
    
    * Сократил для удобства с этого:
    Country country = countryRepository.getByIdOrThrow(userDto.countryId());
    user.setCountry(country);
    на это:
    user.setCountry(countryRepository.getByIdOrThrow(userDto.countryId()));
    
    Обьект country нигде не был задействован, поэтому спокойно сократил
    
    * поправил синтаксис

--- src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
+++ src/main/java/school/faang/user_service/service/user/UserServiceImpl.java
@@ -1,14 +1,16 @@
 package school.faang.user_service.service.user;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.user.CreateUserDto;
 import school.faang.user_service.dto.user.UpdateUserDto;
 import school.faang.user_service.dto.user.UserDto;
-import school.faang.user_service.entity.user.Country;
 import school.faang.user_service.entity.user.User;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.exception.ForbiddenException;
@@ -27,18 +29,29 @@ public class UserServiceImpl implements UserService {
     private final CountryRepository countryRepository;
     private final UserMapper userMapper;
     private final UserContext userContext;
+    @PersistenceContext
+    private EntityManager entityManager;
 
+    @Transactional
     @Override
     public UserDto create(CreateUserDto userDto) {
-        if (userDto.password().length() < minPasswordLength) {
-            throw new DataValidationException("Password should be more than " + minPasswordLength + " symbols!");
-        }
+        createValidation(userDto, minPasswordLength);
         User user = userMapper.toUser(userDto);
-        Country country = countryRepository.getByIdOrThrow(userDto.countryId());
-        user.setCountry(country);
+        user.setActive(true);
+        user.setCountry(countryRepository.getByIdOrThrow(userDto.countryId()));
         user = userRepository.save(user);
         log.info("User {} created", user.getId());
         return userMapper.toUserDto(user);
+    }
+
+    @Transactional
+    @Override
+    public Long delete(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new DataValidationException("User with id " + userId + " not found!"));
+        userRepository.delete(user);
+        log.info("User {} deleted", user.getId());
+        return user.getId();
     }
 
     @Override
@@ -49,8 +62,7 @@ public class UserServiceImpl implements UserService {
         }
         User user = userRepository.getByIdOrThrow(userId);
         userMapper.update(userDto, user);
-        Country country = countryRepository.getByIdOrThrow(userDto.countryId());
-        user.setCountry(country);
+        user.setCountry(countryRepository.getByIdOrThrow(userDto.countryId()));
         user = userRepository.save(user);
         log.info("User {} updated", user.getId());
         return userMapper.toUserDto(user);
@@ -60,5 +72,20 @@ public class UserServiceImpl implements UserService {
     public UserDto getById(long userId) {
         User user = userRepository.getByIdOrThrow(userId);
         return userMapper.toUserDto(user);
+    }
+
+    private void createValidation(CreateUserDto userDto, int minPasswordLength) {
+        if (userRepository.existsByUsername(userDto.username())) {
+            throw new DataValidationException("User with username " + userDto.username() + " already exists!");
+        }
+        if (userDto.password().length() < minPasswordLength) {
+            throw new DataValidationException("Password should be more than " + minPasswordLength + " symbols!");
+        }
+        if (userRepository.existsByEmail(userDto.email())) {
+            throw new DataValidationException("User with email " + userDto.email() + " already exists!");
+        }
+        if (userRepository.existsByPhone(userDto.phone())) {
+            throw new DataValidationException("User with phone " + userDto.phone() + " already exists!");
+        }
     }
 }
