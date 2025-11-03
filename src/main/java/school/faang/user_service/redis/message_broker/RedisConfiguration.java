eeec121b4b5bde198b6724ca8a9b185bb12822a7
Rukk Taras <tarrukk@gmail.com>
2025-11-03 12:54:43 +0300

    Разработал систему блокировки пользователя за нарушения правил приложения в комментариях к постам. + перенес папку с логами из resources в основную папку.

---
+++ src/main/java/school/faang/user_service/redis/message_broker/RedisConfiguration.java
@@ -0,0 +1,61 @@
+package school.faang.user_service.redis.message_broker;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RedisConfiguration {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(
+                redisHost,
+                redisPort
+        );
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    MessageListenerAdapter messageListener(CommenterBannerListener commenterBannerListener) {
+        return new MessageListenerAdapter(commenterBannerListener);
+    }
+
+    @Bean
+    ChannelTopic topic() {
+        return new ChannelTopic("commenter_banner_topic");
+    }
+
+    @Bean
+    RedisMessageListenerContainer redisContainer(
+            MessageListenerAdapter messageListenerAdapter,
+            JedisConnectionFactory jedisConnectionFactory,
+            ChannelTopic topic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(jedisConnectionFactory);
+        container.addMessageListener(messageListenerAdapter, topic);
+        return container;
+    }
+}
