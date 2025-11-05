3b83330c6e1acf4f825274e00733252a1557b054
a.dernovskiy <dernovski@icloud.com>
2025-11-05 17:26:49 +0700

    Добавил тесты для сервиса

--- src/main/java/school/faang/user_service/dto/recommendation/RejectionDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RejectionDto.java
@@ -2,7 +2,6 @@ package school.faang.user_service.dto.recommendation;
 
 import jakarta.validation.constraints.NotBlank;
 
-
 public record RejectionDto(
         @NotBlank(message = "Reason must not be blank")
         String reason) {
