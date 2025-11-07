a9f81986c536dea8ca8513a8dd8ed3509ee1d717
Elya Khamzina <ekkhamzi@mail.ru>
2025-11-07 10:56:20 +0300

    удалила 1 метод feature-BJS2-96857-added-analytics

--- src/main/java/school/faang/user_service/publisher/EventsPublisher.java
+++ src/main/java/school/faang/user_service/publisher/EventsPublisher.java
@@ -23,11 +23,6 @@ public class EventsPublisher {
         }
     }
 
-    public void publishProfileView(long profileId, long viewerId) {
-        log.info("Publishing PROFILE_VIEW event: profileId={}, viewerId={}", profileId, viewerId);
-        sendEvent(ANALYTICS_TOPIC, new EventDto(viewerId, profileId, "PROFILE_VIEW"));
-    }
-
     public void publishFollow(long followerId, long followingId) {
         log.info("Publishing FOLLOWER event: followerId={}, followingId={}", followerId, followingId);
         sendEvent(ANALYTICS_TOPIC, new EventDto(followerId, followingId, "FOLLOWER"));
