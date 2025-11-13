9a3cc515a72cec9eb89f50e808f468666f2476d7
teymuruae <baratov.teymur@yandex.ru>
2025-11-13 22:33:27 +0300

    feature-BJS2-97042 mentorship offer event notification publisher review fixes

--- src/main/java/school/faang/user_service/publisher/MentorshipOfferedEventPublisher.java
+++ src/main/java/school/faang/user_service/publisher/MentorshipOfferedEventPublisher.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +7,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.event.mentorship.MentorshipOfferedEvent;
-import school.faang.user_service.exception.EventPublishingException;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -18,36 +15,27 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class MentorshipOfferedEventPublisher {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final ObjectMapper objectMapper;
+    private final KafkaTemplate<String, MentorshipOfferedEvent> kafkaTemplate;
 
     @Value("${kafka.topic.mentorship-offer}")
     private String mentorshipOfferTopic;
 
     public void publish(MentorshipOfferedEvent mentorshipOfferedEvent) {
-        try {
+        String key = mentorshipOfferedEvent.mentorId().toString();
 
-            String key = mentorshipOfferedEvent.mentorId().toString();
-            String jsonBody = objectMapper.writeValueAsString(mentorshipOfferedEvent);
-            CompletableFuture<SendResult<String, String>> future
-                    = kafkaTemplate.send(mentorshipOfferTopic, key, jsonBody);
+        CompletableFuture<SendResult<String, MentorshipOfferedEvent>> future
+                = kafkaTemplate.send(mentorshipOfferTopic, key, mentorshipOfferedEvent);
 
-            future.whenComplete((result, ex) -> {
-                if (ex != null) {
-                    log.error("Failed to send mentorship offered event: Event {}, Mentor {} Mentee {}",
-                            mentorshipOfferedEvent.mentorshipRequestId(), mentorshipOfferedEvent.mentorId(),
-                            mentorshipOfferedEvent.menteeId());
-                } else {
-                    log.info("Sent mentorship offered event: Event {}, Mentor {} Mentee {}",
-                            mentorshipOfferedEvent.mentorshipRequestId(), mentorshipOfferedEvent.mentorId(),
-                            mentorshipOfferedEvent.menteeId());
-                }
-            });
-        } catch (JsonProcessingException e) {
-            String errorMessage = "JSON serialization failed for event: %d"
-                    .formatted(mentorshipOfferedEvent.mentorshipRequestId());
-            log.error(errorMessage);
-            throw new EventPublishingException(errorMessage);
-        }
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("Failed to send mentorship offered event: Event {}, Mentor {} Mentee {}",
+                        mentorshipOfferedEvent.mentorshipRequestId(), mentorshipOfferedEvent.mentorId(),
+                        mentorshipOfferedEvent.menteeId());
+            } else {
+                log.info("Sent mentorship offered event: Event {}, Mentor {} Mentee {}",
+                        mentorshipOfferedEvent.mentorshipRequestId(), mentorshipOfferedEvent.mentorId(),
+                        mentorshipOfferedEvent.menteeId());
+            }
+        });
     }
 }
--- src/test/java/school/faang/user_service/publisher/MentorshipOfferedEventPublisherTest.java
+++ src/test/java/school/faang/user_service/publisher/MentorshipOfferedEventPublisherTest.java
@@ -1,12 +1,13 @@
 package school.faang.user_service.publisher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -15,9 +16,7 @@ import school.faang.user_service.event.mentorship.MentorshipOfferedEvent;
 
 import java.util.concurrent.CompletableFuture;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.never;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -31,10 +30,12 @@ public class MentorshipOfferedEventPublisherTest {
             .mentorshipRequestId(1L)
             .build();
 
+    @Captor
+    private ArgumentCaptor<MentorshipOfferedEvent> mentorshipOfferedEventArgumentCaptor =
+            ArgumentCaptor.forClass(MentorshipOfferedEvent.class);
+
     @Mock
-    private ObjectMapper objectMapper;
-    @Mock
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, MentorshipOfferedEvent> kafkaTemplate;
 
     @InjectMocks
     private MentorshipOfferedEventPublisher mentorshipOfferedEventPublisher;
@@ -46,39 +47,17 @@ public class MentorshipOfferedEventPublisherTest {
     }
 
     @Test
-    void testPublish() throws JsonProcessingException {
-        String json = "json";
-        CompletableFuture<SendResult<String, String>> future = CompletableFuture.completedFuture(null);
+    void testPublish() {
+        CompletableFuture<SendResult<String, MentorshipOfferedEvent>> future = CompletableFuture.completedFuture(null);
 
-        when(kafkaTemplate.send(mentorshipOfferTopic, event.mentorId().toString(), json)).thenReturn(future);
-        when(objectMapper.writeValueAsString(event)).thenReturn(json);
+        when(kafkaTemplate.send(mentorshipOfferTopic, event.mentorId().toString(), event)).thenReturn(future);
 
         mentorshipOfferedEventPublisher.publish(event);
 
-        verify(kafkaTemplate).send(mentorshipOfferTopic, event.mentorId().toString(), json);
-    }
+        verify(kafkaTemplate).send(Mockito.eq(mentorshipOfferTopic), Mockito.eq(event.mentorId().toString()),
+                mentorshipOfferedEventArgumentCaptor.capture());
 
-    @Test
-    void testPublish_ShouldThrowRuntimeExceptionWhenJsonProcessingFails() throws JsonProcessingException {
-        when(objectMapper.writeValueAsString(event)).thenThrow(JsonProcessingException.class);
-
-        assertThrows(RuntimeException.class, () -> mentorshipOfferedEventPublisher.publish(event));
-
-        verify(kafkaTemplate, never()).send(anyString(), anyString());
-    }
-
-
-    @Test
-    void testPublish_WhenKafkaSendFails() throws JsonProcessingException {
-        String json = "json";
-        CompletableFuture<SendResult<String, String>> future = new CompletableFuture<>();
-        future.completeExceptionally(new RuntimeException("Kafka error"));
-
-        when(kafkaTemplate.send(mentorshipOfferTopic, event.mentorId().toString(), json)).thenReturn(future);
-        when(objectMapper.writeValueAsString(event)).thenReturn(json);
-
-        mentorshipOfferedEventPublisher.publish(event);
-
-        verify(kafkaTemplate).send(mentorshipOfferTopic, event.mentorId().toString(), json);
+        MentorshipOfferedEvent capturedEvent = mentorshipOfferedEventArgumentCaptor.getValue();
+        assertEquals(event.mentorshipRequestId(), capturedEvent.mentorshipRequestId());
     }
 }
