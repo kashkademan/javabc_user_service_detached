275b464ccbe6cb38758ff577b7ed1a71dd0b53e5
ayazs <ayaz-22@yandex.ru>
2025-11-14 23:42:32 +0500

    BJS2-97057 done

---
+++ src/main/java/school/faang/user_service/publisher/RecommendationReceivedEventPublisher.java
@@ -0,0 +1,34 @@
+package school.faang.user_service.publisher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import school.faang.user_service.dto.recommendation.RecommendationReceivedEvent;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RecommendationReceivedEventPublisher implements MessagePublisher {
+
+    @Value("${spring.data.redis.chanel.recommendation}")
+    private String recommendationChannelName;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void publish(RecommendationReceivedEvent event) {
+        try {
+            redisTemplate.convertAndSend(recommendationChannelName, event);
+
+            log.info("Recommendation event published: eventId={}", event.recommendationId());
+
+        } catch (Exception e) {
+            log.error("Failed to publish recommendation event: {}", e.getMessage(), e);
+            throw new RuntimeException("Failed to publish recommendation event", e);
+        }
+    }
+}
