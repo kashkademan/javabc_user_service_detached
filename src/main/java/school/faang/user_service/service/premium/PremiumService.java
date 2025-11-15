1de3ba0fc1b0c2279d2a7113d44dd83716ec7330
teymuruae <baratov.teymur@yandex.ru>
2025-11-15 13:51:17 +0300

    feature-BJS2-96852 remove expired premium accesses done

---
+++ src/main/java/school/faang/user_service/service/premium/PremiumService.java
@@ -0,0 +1,5 @@
+package school.faang.user_service.service.premium;
+
+public interface PremiumService {
+    void deleteExpired();
+}
---
+++ src/main/java/school/faang/user_service/service/premium/PremiumServiceImpl.java
@@ -0,0 +1,68 @@
+package school.faang.user_service.service.premium;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import school.faang.user_service.entity.premium.Premium;
+import school.faang.user_service.repository.premium.PremiumRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PremiumServiceImpl implements PremiumService {
+
+    private final PremiumRepository premiumRepository;
+    private final ExecutorService deletePremiumPool;
+
+    @Value("${premium.max-delete-per-thread}")
+    private int maxDeletePerThread;
+    @Value("${premium.delete-expired-timeout}")
+    private int deleteExpiredTimeout;
+
+    @Override
+    public void deleteExpired() {
+        List<Long> expiredPremiumAccesses = premiumRepository.findAllByEndDateBefore(LocalDateTime.now())
+                .stream()
+                .map(Premium::getId)
+                .toList();
+
+        log.info("Found {} expired premium accesses: {}", expiredPremiumAccesses.size(), expiredPremiumAccesses);
+
+        List<List<Long>> parts = new ArrayList<>();
+        for (int i = 0; i < expiredPremiumAccesses.size(); i += maxDeletePerThread) {
+            int end = Math.min(i + maxDeletePerThread, expiredPremiumAccesses.size());
+            parts.add(expiredPremiumAccesses.subList(i, end));
+        }
+
+        List<CompletableFuture<Void>> futures = parts.stream()
+                .map(part -> CompletableFuture.runAsync(
+                        () -> {
+                            log.info("Deleting premium: {}", part);
+                            premiumRepository.deleteAllById(part);
+                        },
+                        deletePremiumPool
+                ))
+                .toList();
+
+        try {
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                    .get(deleteExpiredTimeout, TimeUnit.SECONDS);
+        } catch (TimeoutException e) {
+            log.error("Deletion timed out. {}", e.getMessage());
+            futures.forEach(future -> future.cancel(true));
+        } catch (Exception e) {
+            log.error("Error during deletion. {}", e.getMessage());
+        }
+
+        log.info("{} expired premium accesses have been deleted", expiredPremiumAccesses.size());
+    }
+}
