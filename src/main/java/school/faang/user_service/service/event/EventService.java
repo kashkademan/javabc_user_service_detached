d9e29f28438523e5f5aef29cead1c2dddcd2d9d0
Viks <viktor.naroznoi@gmail.com>
2025-11-17 21:22:37 +0200

    --BJS2-96825 - corrections based on feedback

--- src/main/java/school/faang/user_service/service/event/EventService.java
+++ src/main/java/school/faang/user_service/service/event/EventService.java
@@ -1,47 +1,57 @@
 package school.faang.user_service.service.event;
 
-import lombok.RequiredArgsConstructor;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
-import school.faang.user_service.config.execservice.AsyncExecutor;
-import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.entity.event.EventStatus;
 import school.faang.user_service.repository.event.EventRepository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class EventService {
 
     private final EventRepository eventRepository;
-    private final AsyncExecutor asyncExecutor;
+    private final ThreadPoolTaskExecutor executor;
+    private final ChunkDeletionService chunkDeletionService;
 
-    public void clearPastEvents(int chunkSize) {
-        List<Long> completedEventIds = eventRepository.findAllIdByStatus(EventStatus.COMPLETED);
-        if (completedEventIds.isEmpty()) {
-            log.info("No completed events found.");
-            return;
-        }
-        List<List<Long>> chunks = partitionList(completedEventIds, chunkSize);
-
-        ThreadPoolTaskExecutor executor = asyncExecutor.getAsyncExecutor(chunks.size());
-
-        chunks.forEach((chunk) -> executor.execute(() -> eventRepository.deleteAllById(chunk)));
+    public EventService(
+            EventRepository eventRepository,
+            @Qualifier("expiredEventTaskExecutor") ThreadPoolTaskExecutor executor,
+            ChunkDeletionService chunkDeletionService
+    ) {
+        this.eventRepository = eventRepository;
+        this.executor = executor;
+        this.chunkDeletionService = chunkDeletionService;
     }
 
-    private List<List<Long>> partitionList(List<Long> list, int chunkSize) {
-        List<List<Long>> parts = new ArrayList<>((list.size() + chunkSize - 1) / chunkSize);
-        for (int i = 0; i < list.size(); i += chunkSize) {
-            parts.add(list.subList(i, Math.min(list.size(), i + chunkSize)));
+    public void clearPastEvents(int chunkSize) {
+
+        int pageNumber = 0;
+
+        while (true) {
+            Page<Long> page = eventRepository.findAllIdByStatus(
+                    EventStatus.COMPLETED,
+                    PageRequest.of(pageNumber, chunkSize)
+            );
+
+            if (page.isEmpty()) {
+                log.info("No more completed events to delete.");
+                break;
+            }
+
+            List<Long> idsToDelete = page.getContent();
+
+            executor.execute(() -> {
+                log.debug("Deleting {} events...", idsToDelete.size());
+                chunkDeletionService.deleteChunk(idsToDelete);
+            });
+
+            pageNumber++;
         }
-        return parts;
     }
 }
