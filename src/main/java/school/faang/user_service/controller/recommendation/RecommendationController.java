6baeb30010eac09fb0143a5920415d34d6ca146e
Viks <viktor.naroznoi@gmail.com>
2025-11-17 13:05:49 +0200

    --BJS2-96430 - corrections based on feedback

--- src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
+++ src/main/java/school/faang/user_service/controller/recommendation/RecommendationController.java
@@ -1,9 +1,12 @@
 package school.faang.user_service.controller.recommendation;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,23 +29,23 @@ public class RecommendationController {
     private final RecommendationService recommendationService;
 
     @PostMapping
-    public ResponseEntity<RecommendationDto> create(@RequestBody CreateRecommendationDto recommendationDto) {
+    public ResponseEntity<RecommendationDto> create(@Valid @RequestBody CreateRecommendationDto recommendationDto) {
         return ResponseEntity.ok(recommendationService.create(recommendationDto));
     }
 
     @PutMapping("/{recommendationId}")
-    public ResponseEntity<RecommendationDto> update(long recommendationId,
+    public ResponseEntity<RecommendationDto> update(@PathVariable long recommendationId,
                                                     @RequestBody UpdateRecommendationDto recommendationDto) {
         return ResponseEntity.ok(recommendationService.update(recommendationId, recommendationDto));
     }
 
     @DeleteMapping("/{recommendationId}")
-    public ResponseEntity<Void> delete(long recommendationId) {
+    public ResponseEntity<Void> delete(@PathVariable long recommendationId) {
         recommendationService.delete(recommendationId);
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/search")
+    @GetMapping("/search")
     public ResponseEntity<List<RecommendationDto>> getByFilters(
             @RequestBody RecommendationFilterDto filters) {
         return ResponseEntity.ok(recommendationService.getByFilters(filters));
--- src/main/java/school/faang/user_service/dto/recommendation/RecommendationFilterDto.java
+++ src/main/java/school/faang/user_service/dto/recommendation/RecommendationFilterDto.java
@@ -1,12 +1,8 @@
 package school.faang.user_service.dto.recommendation;
 
-import jakarta.validation.constraints.NotNull;
-
 public record RecommendationFilterDto(
         String contentContains,
-        @NotNull(message = "Recommendation author ID cannot be absent")
         Long authorId,
-        @NotNull(message = "Recommendation received ID cannot be absent")
         Long receiverId
 ) {
 }
