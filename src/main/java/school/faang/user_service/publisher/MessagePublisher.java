1f03c0bc1be3620c134a5472ea9c8b94a38f980c
ayazs <ayaz-22@yandex.ru>
2025-11-17 16:53:34 +0500

    BJS2-96890 UserServiceImpl changed, injected List<MessagePublisher<Object>> publishers;

--- src/main/java/school/faang/user_service/publisher/MessagePublisher.java
+++ src/main/java/school/faang/user_service/publisher/MessagePublisher.java
@@ -1,7 +1,5 @@
 package school.faang.user_service.publisher;
 
-import school.faang.user_service.dto.event.SearchAppearanceEvent;
-
 public interface MessagePublisher<T> {
 
     void publish(T event);
