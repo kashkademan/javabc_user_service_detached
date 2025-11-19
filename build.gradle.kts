61ee1847ed2a5b948b6004d65894ec772bd11abc
Scherbi <shherbakov99ilya@mail.ru>
2025-11-19 21:25:31 +0300

    BJS2-97353 doit Int Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,6 @@ dependencies {
      * Utils & Logging
      */
     implementation("org.imgscalr:imgscalr-lib:4.2")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation(platform("com.fasterxml.jackson:jackson-bom:2.15.2"))
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("com.fasterxml.jackson.core:jackson-annotations")
