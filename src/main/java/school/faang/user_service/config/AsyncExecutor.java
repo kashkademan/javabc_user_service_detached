2ec381f8be2c70693425bd1f7ebb4e0dd7bcdf10
Viks <viktor.naroznoi@gmail.com>
2025-11-21 11:42:06 +0200

    --This branch is for demonstration purposes

--- src/main/java/school/faang/user_service/config/AsyncExecutor.java
+++ src/main/java/school/faang/user_service/config/AsyncExecutor.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.config.execservice;
+package school.faang.user_service.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
