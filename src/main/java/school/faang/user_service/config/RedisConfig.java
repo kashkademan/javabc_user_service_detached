519da8fc35de6d4a25760a8379f2d1f62108362f
Viks <viktor.naroznoi@gmail.com>
2025-11-24 03:04:09 +0200

    --BJS2-97084 - Corrections based on feedback

--- src/main/java/school/faang/user_service/config/RedisConfig.java
+++ src/main/java/school/faang/user_service/config/RedisConfig.java
@@ -1,16 +1,15 @@
 package school.faang.user_service.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-@RequiredArgsConstructor
 public class RedisConfig {
 
     @Value("${spring.data.redis.host}")
@@ -26,11 +25,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 }
--- src/main/java/school/faang/user_service/messaging/GoalCompletedEventPublisher.java
+++ src/main/java/school/faang/user_service/messaging/GoalCompletedEventPublisher.java
@@ -3,12 +3,13 @@ package school.faang.user_service.messaging;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import school.faang.user_service.dto.event.GoalCompletedEvent;
-import school.faang.user_service.exception.DataValidationException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class GoalCompletedEventPublisher {
@@ -19,13 +20,9 @@ public class GoalCompletedEventPublisher {
     @Value("${spring.data.redis.channel.goal_completed}")
     private String channel;
 
-    public void publish(GoalCompletedEvent goalCompletedEvent) {
-        String json;
-        try {
-            json = objectMapper.writeValueAsString(goalCompletedEvent);
-        } catch (JsonProcessingException e) {
-            throw new DataValidationException("Event to JSON conversion error.");
-        }
-        redisTemplate.convertAndSend(channel, json);
+    public void publish(GoalCompletedEvent goalCompletedEvent) throws JsonProcessingException {
+        log.info("Publishing goal completed event notification");
+        redisTemplate.convertAndSend(channel, goalCompletedEvent);
+        log.info("Goal completed event notification sent");
     }
 }
