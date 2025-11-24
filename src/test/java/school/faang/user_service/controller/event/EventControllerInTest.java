eec3c83b83b950108e47fd4ccb4a7128e3c0ac6a
Scherbi <shherbakov99ilya@mail.ru>
2025-11-24 03:02:43 +0300

    BJS2-97356 refactor test and method

--- src/test/java/school/faang/user_service/controller/event/EventControllerInTest.java
+++ src/test/java/school/faang/user_service/controller/event/EventControllerInTest.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.controller.event;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 import school.faang.user_service.ApplicationContextTest;
 import school.faang.user_service.dto.event.EventCreateDto;
 import school.faang.user_service.dto.event.EventDto;
@@ -17,8 +17,10 @@ import school.faang.user_service.dto.event.EventUpdateDto;
 import school.faang.user_service.entity.event.Event;
 import school.faang.user_service.entity.event.EventStatus;
 import school.faang.user_service.entity.event.EventType;
+import school.faang.user_service.entity.user.Country;
 import school.faang.user_service.entity.user.User;
 import school.faang.user_service.repository.event.EventRepository;
+import school.faang.user_service.repository.user.CountryRepository;
 import school.faang.user_service.repository.user.UserRepository;
 
 import java.time.LocalDateTime;
@@ -43,6 +45,9 @@ public class EventControllerInTest extends ApplicationContextTest {
     private UserRepository userRepository;
 
     @Autowired
+    private CountryRepository countryRepository;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
@@ -55,11 +60,17 @@ public class EventControllerInTest extends ApplicationContextTest {
     void setUp() {
         eventRepository.deleteAll();
         userRepository.deleteAll();
+        countryRepository.deleteAll();
+
+        Country country = new Country();
+        country.setTitle("TestСountry_");
+        countryRepository.save(country);
 
         owner = User.builder()
                 .username("event_owner")
                 .email("owner@test.com")
                 .password("password123")
+                .country(country)
                 .active(true)
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
@@ -73,6 +84,7 @@ public class EventControllerInTest extends ApplicationContextTest {
                 .endDate(LocalDateTime.now().plusDays(2))
                 .type(EventType.WEBINAR)
                 .owner(owner)
+                .location("Bryansk")
                 .status(EventStatus.PLANNED)
                 .build();
         eventRepository.save(event);
@@ -80,6 +92,7 @@ public class EventControllerInTest extends ApplicationContextTest {
 
     @Test
     void create_withValidData_shouldReturnEventDto() throws Exception {
+        Long currentUserId = owner.getId();
         EventCreateDto createDto = EventCreateDto.builder()
                 .title("New Event")
                 .description("New Description")
@@ -91,18 +104,20 @@ public class EventControllerInTest extends ApplicationContextTest {
 
         mockMvc.perform(post("/events")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header("x-user-id", currentUserId.toString())
                         .content(objectMapper.writeValueAsString(createDto)))
                 .andExpectAll(
                         status().isCreated(),
                         jsonPath("$.title").value("New Event"),
                         jsonPath("$.description").value("New Description"),
-                        jsonPath("$.type").value("CONFERENCE"),
-                        jsonPath("$.status").value("ACTIVE")
-                );
+                        jsonPath("$.type").value("WEBINAR"),
+                        jsonPath("$.status").value("PLANNED")
+            );
     }
 
     @Test
     void update_withValidData_shouldUpdateEvent() throws Exception {
+        Long currentUserId = owner.getId();
         EventUpdateDto updateDto = EventUpdateDto.builder()
                 .title("Updated Event")
                 .description("Updated Description")
@@ -116,17 +131,22 @@ public class EventControllerInTest extends ApplicationContextTest {
 
         mockMvc.perform(patch("/events/{eventId}", event.getId())
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header("x-user-id", currentUserId.toString())
                         .content(objectMapper.writeValueAsString(updateDto)))
                 .andExpectAll(
                         status().isOk(),
                         jsonPath("$.title").value("Updated Event"),
                         jsonPath("$.description").value("Updated Description")
-                );
+            );
     }
 
     @Test
     void delete_withValidData_shouldDeleteEvent() throws Exception {
-        mockMvc.perform(delete("/events/{eventId}", event.getId()))
+        Long currentUserId = owner.getId();
+        Event event1 = eventRepository.findById(event.getId()).get();
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + event1.getTitle());
+        mockMvc.perform(delete("/events/{eventId}", event.getId())
+                        .header("x-user-id", currentUserId.toString()))
                 .andExpect(status().isNoContent());
 
         assertThat(eventRepository.findById(event.getId())).isEmpty();
@@ -134,12 +154,14 @@ public class EventControllerInTest extends ApplicationContextTest {
 
     @Test
     void filters_withValidData_shouldReturnFilteredEvents() throws Exception {
+        Long currentUserId = owner.getId();
         EventFilterDto filterDto = EventFilterDto.builder()
-                .titleContains("Start")
+                .titleContains("Start Event")
                 .build();
 
         MvcResult result = mockMvc.perform(post("/events/filters")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header("x-user-id", currentUserId.toString())
                         .content(objectMapper.writeValueAsString(filterDto)))
                 .andExpect(status().isOk())
                 .andReturn();
