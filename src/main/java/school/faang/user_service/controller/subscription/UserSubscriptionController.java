6dd7289c0fbac47d1d131b9f2ba6cf2789aa5536
Yasin <yasin.akhmadov555@inbox.ru>
2025-11-25 22:12:45 +0300

    Написал конфигурацию Redis для публикаторов и публикатор FollowerEventPublisher для события о подписке (FollowerEvent)

--- src/main/java/school/faang/user_service/controller/subscription/UserSubscriptionController.java
+++ src/main/java/school/faang/user_service/controller/subscription/UserSubscriptionController.java
@@ -1,12 +1,21 @@
 package school.faang.user_service.controller.subscription;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import school.faang.user_service.config.context.UserContext;
 import school.faang.user_service.dto.CountResponse;
-import school.faang.user_service.event.FollowerEvent;
 import school.faang.user_service.dto.user.UserDto;
 import school.faang.user_service.dto.user.UserFiltersDto;
+import school.faang.user_service.event.FollowerEvent;
 import school.faang.user_service.exception.DataValidationException;
 import school.faang.user_service.exception.ForbiddenException;
 import school.faang.user_service.publisher.FollowerEventPublisher;
@@ -18,52 +27,50 @@ import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/subscriptions")
+@Validated
 public class UserSubscriptionController {
     private final UserSubscriptionService userSubscriptionService;
     private final UserContext userContext;
     private final FollowerEventPublisher eventPublisher;
 
-    public void followUser(long followeeId) {
-        ensureUserIdValid(followeeId);
+    @PostMapping("/following")
+    public void followUser(@RequestParam @Positive long followeeId) {
         long followerId = userContext.getUserId();
         ensureFollowerAndFolloweeNotTheSameUser(followerId, followeeId);
         userSubscriptionService.followUser(followerId, followeeId);
         eventPublisher.publish(new FollowerEvent(followeeId, followerId, LocalDateTime.now()));
     }
 
-    public void unfollowUser(long followeeId) {
-        ensureUserIdValid(followeeId);
+    @PostMapping("/unfollowing")
+    public void unfollowUser(@RequestParam @Positive long followeeId) {
         long followerId = userContext.getUserId();
         ensureFollowerAndFolloweeNotTheSameUser(followerId, followeeId);
         userSubscriptionService.unfollowUser(followerId, followeeId);
     }
 
-    public CountResponse getFollowersCount(long followeeId) {
-        ensureUserIdValid(followeeId);
+    @GetMapping("/{followeeId}/followers/count")
+    public CountResponse getFollowersCount(@PathVariable @Positive long followeeId) {
         return userSubscriptionService.getFollowersCount(followeeId);
     }
 
-    public CountResponse getFolloweesCount(long followerId) {
-        ensureUserIdValid(followerId);
+    @GetMapping("/{followerId}/followees/count")
+    public CountResponse getFolloweesCount(@PathVariable @Positive long followerId) {
         return userSubscriptionService.getFolloweesCount(followerId);
     }
 
-    public List<UserDto> getFollowers(long followeeId, UserFiltersDto userFiltersDto) {
-        ensureUserIdValid(followeeId);
+    @GetMapping("/{followeeId}/followers")
+    public List<UserDto> getFollowers(@PathVariable @Positive long followeeId,
+                                      @Valid @RequestBody UserFiltersDto userFiltersDto) {
         ensureUserFiltersDtoValid(userFiltersDto);
         return userSubscriptionService.getFollowers(followeeId, userFiltersDto);
     }
 
-    public List<UserDto> getFollowees(long followerId, UserFiltersDto userFiltersDto) {
-        ensureUserIdValid(followerId);
+    @GetMapping("/{followerId}/followees")
+    public List<UserDto> getFollowees(@PathVariable @Positive long followerId,
+                                      @Valid @RequestBody UserFiltersDto userFiltersDto) {
         ensureUserFiltersDtoValid(userFiltersDto);
         return userSubscriptionService.getFollowees(followerId, userFiltersDto);
-    }
-
-    private void ensureUserIdValid(long userId) {
-        if (userId <= 0) {
-            throw new DataValidationException("user id cannot be equal to or less than zero");
-        }
     }
 
     private void ensureFollowerAndFolloweeNotTheSameUser(long followerId, long followeeId) {
@@ -74,21 +81,8 @@ public class UserSubscriptionController {
 
     private void ensureUserFiltersDtoValid(UserFiltersDto userFiltersDto) {
         Objects.requireNonNull(userFiltersDto, "user filters cannot be null");
-        String namePattern = userFiltersDto.namePattern();
-        String phoneNumber = userFiltersDto.phoneNumber();
-        int experienceMin = userFiltersDto.experienceMin();
-        int experienceMax = userFiltersDto.experienceMax();
 
-        if (namePattern == null || namePattern.isBlank()) {
-            throw new DataValidationException("user filters must include full name pattern");
-        }
-        if (phoneNumber == null || phoneNumber.isBlank()) {
-            throw new DataValidationException("user filters must include full phone number");
-        }
-        if (experienceMin < 0 || experienceMax < 0) {
-            throw new DataValidationException("experience min or experience max cannot be less than zero");
-        }
-        if (experienceMin > experienceMax) {
+        if (userFiltersDto.experienceMin() > userFiltersDto.experienceMax()) {
             throw new DataValidationException("experience min cannot be greater than experience max");
         }
     }
--- src/main/java/school/faang/user_service/dto/user/UserFiltersDto.java
+++ src/main/java/school/faang/user_service/dto/user/UserFiltersDto.java
@@ -1,9 +1,21 @@
 package school.faang.user_service.dto.user;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+@NotNull(message = "user filters cannot be null")
 public record UserFiltersDto(
+        @NotBlank(message = "name pattern cannot be null, empty or a space")
         String namePattern,
+
+        @NotBlank(message = "phone number cannot be null, empty or a space")
         String phoneNumber,
+
+        @PositiveOrZero(message = "minimal experience cannot be less than zero")
         int experienceMin,
+
+        @PositiveOrZero(message = "maximum experience cannot be less than zero")
         int experienceMax
 ) {
 }
