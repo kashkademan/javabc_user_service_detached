0f9b626a3335a044cc0226c560c054b24bfe4ba2
Rukk Taras <tarrukk@gmail.com>
2025-12-02 12:47:56 +0300

    Исправил согласно комментариям тим-лида

--- src/main/java/school/faang/user_service/controller/user/UserController.java
+++ src/main/java/school/faang/user_service/controller/user/UserController.java
@@ -1,5 +1,6 @@
 package school.faang.user_service.controller.user;
 
+import io.micrometer.common.util.StringUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
