639c3a2ad01c6b4897f771bdd9de19068ea592a9
Yasin <yasin.akhmadov555@inbox.ru>
2026-01-09 14:26:09 +0300

    Теперь интеграционный тест лежит в правильном пакете

--- src/test/java/school/faang/user_service/publisher/subscription/FollowerEventPublisherIntegrationTest.java
+++ src/test/java/school/faang/user_service/publisher/subscription/FollowerEventPublisherIntegrationTest.java
@@ -1,4 +1,4 @@
-package school.faang.user_service.controller.subscription;
+package school.faang.user_service.publisher.subscription;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
